5eb86b07f46c74669bad7f816819f40da9f51e5d
allegrogiken <vvani06+dev@gmail.com>
2021-01-11 14:38:16 +0900

    ModInt の alias this が雑で mod 外れてる時があった

--- home/snippets/modInt.d
+++ home/snippets/modInt.d
@@ -1,13 +1,12 @@
 struct ModInt(uint MD) if (MD < int.max) {
   ulong v;
-  alias v this;
   this(int v) {this(long(v));}
   this(long v) {this.v = (v%MD+MD)%MD;}
   static auto normS(ulong x) {return (x<MD)?x:x-MD;}
   static auto make(ulong x) {ModInt m; m.v = x; return m;}
   auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r));}
   auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r));}
-  auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r%MD).to!ulong);}
+  auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}
   auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}
   auto opBinary(string op:"/")(ModInt r) const {return this*inv(r);}
   static ModInt inv(ModInt x) {return x^^(MD-2);};
