e1e789de29f1c61783f5d0a1daa05c3932f8c5ac
allegrogiken <vvani06+dev@gmail.com>
2021-03-14 23:48:18 +0900

    AHC001

--- home/heuristic/0306_ahc001/a.d
+++ home/heuristic/0306_ahc001/a.d
@@ -3,25 +3,56 @@ void main() {
 }
 
 void problem() {
+  enum MAX = 10_000;
+  enum MIN = 0;
   alias Desire = Tuple!(long, "x", long, "y", long, "s");
   alias Ad = Tuple!(long, "sx", long, "sy", long, "ex", long, "ey");
-  enum real GETA = 10^^9;
+
+  long[] minimumDistances(Desire[] desires) {
+    auto ret = new long[](desires.length);
+    ret[] = long.max;
+
+    foreach(i; 0..desires.length-1) {
+      foreach(j; i+1..desires.length) {
+        real d = (desires[i].x - desires[j].x)^^2 + (desires[i].y - desires[j].y) ^^ 2;
+        long ld = d.sqrt.to!long;
+        ret[i] = ret[i].min(ld);
+        ret[j] = ret[j].min(ld);
+      }
+    }
+
+    return ret;
+  }
 
   Ad[] solve(Desire[] desires) {
-    return Ad(0, 0, 10000, 10000).repeat(desires.length).array;
+    auto ads = desires.map!(d => Ad(d.x, d.y, d.x, d.y)).array;
+    auto distances = minimumDistances(desires);
+
+    foreach(i, d; distances) {
+      auto r = cast(real)(d / 2) / 2f.sqrt;
+      long l = r.to!long;
+
+      ads[i].sx = max(MIN, ads[i].sx - l);
+      ads[i].ex = min(MAX, ads[i].ex + l);
+      ads[i].sy = max(MIN, ads[i].sy - l);
+      ads[i].ey = min(MAX, ads[i].ey + l);
+    }
+
+    return ads;
   }
 
   long calculate(Desire[] desires, Ad[] ads) {
+    enum real GETA = 10^^9;
     long ans;
     foreach(i; 0..desires.length) {
       auto d = desires[i];
       auto a = ads[i];
-      if (a.sx > d.x || a.ex < d.x || a.sy > d.y || a.ey < d.y) continue;
+      if (a.sx > d.x || a.ex <= d.x || a.sy > d.y || a.ey <= d.y) continue;
 
       real seg = (a.ex - a.sx) * (a.ey - a.sy);
       real des = d.s;
       real ratio = 1.to!real - min(des, seg) / max(des, seg);
-      ans += GETA * (1.to!real - ratio.pow(2));
+      ans += (GETA * (1.to!real - ratio.pow(2))).to!long;
     }
 
     return (ans / desires.length).to!long;
@@ -31,9 +62,12 @@ void problem() {
   auto desires = N.iota.map!(i => Desire(scan!long, scan!long, scan!long)).array;
   auto ads = solve(desires);
 
-  debug {
-    calculate(desires, ads).deb;
+  debug "========================= OUTPUT =========================".deb;
+  foreach(a; ads) {
+    writefln("%s %s %s %s", a.sx, a.sy, a.ex, a.ey);
   }
+  debug "========================= SCORE ==========================".deb;
+  debug calculate(desires, ads).deb;
 }
 
 // ----------------------------------------------
