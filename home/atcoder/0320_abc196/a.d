5b6338578742cb3cf5ae56e8555ac163f5694e2f
allegrogiken <vvani06+dev@gmail.com>
2021-03-21 17:57:06 +0900

    ABC196 ABCD完

---
+++ home/atcoder/0320_abc196/a.d
@@ -0,0 +1,50 @@
+void main() {
+  debug {
+    "==================================".writeln;
+    while(true) {
+      auto bench =  benchmark!problem(1);
+      "<<< Process time: %s >>>".writefln(bench[0]);
+      "==================================".writeln;
+    }
+  } else {
+    problem();
+  }
+}
+
+void problem() {
+  auto A = scan!long;
+  auto B = scan!long;
+  auto C = scan!long;
+  auto D = scan!long;
+
+  auto solve() {
+    long ans = -1000;
+    foreach(x; A..B+1) {
+      foreach(y; C..D+1) {
+        ans = ans.max(x - y);
+      }
+    }
+    return ans;
+  }
+
+  static if (is(ReturnType!(solve) == void)) solve(); else solve().writeln;
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time;
+T[][] combinations(T)(T[] s, in int m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+ulong MOD = 10^^9 + 7;
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
---
+++ home/atcoder/0320_abc196/c.d
@@ -0,0 +1,53 @@
+void main() {
+  debug {
+    "==================================".writeln;
+    while(true) {
+      auto bench =  benchmark!problem(1);
+      "<<< Process time: %s >>>".writefln(bench[0]);
+      "==================================".writeln;
+    }
+  } else {
+    problem();
+  }
+}
+
+void problem() {
+  auto N = scan!long;
+
+  auto solve() {
+    long ans;
+
+    foreach(long keta; 0..7) {
+      long geta = 10^^(keta + 1);
+      foreach(long x; 10^^keta..10^^(keta + 1)) {
+        // [x, geta*x + x].deb;
+        if (geta*x + x > N) return ans;
+
+        ans++;
+      }
+    }
+
+    return ans;
+  }
+
+  static if (is(ReturnType!(solve) == void)) solve(); else solve().writeln;
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time;
+T[][] combinations(T)(T[] s, in int m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+ulong MOD = 10^^9 + 7;
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
