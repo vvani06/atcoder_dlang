3897e2492f13f70f015a0eb2ea34b52759ba20bb
allegrogiken <vvani06+dev@gmail.com>
2021-05-11 20:35:15 +0900

    upsolve

--- home/atcoder/0508_abc200/d.d
+++ home/atcoder/0508_abc200/d.d
@@ -5,28 +5,26 @@ void problem() {
   auto A = scan!long(N);
 
   auto solve() {
-    long[][long] okA = [0: []];
-    
-    foreach(long i, a; A) {
-      foreach(k; okA.keys()) {
-        const m = (k + a) % 200;
-        if (m in okA && !okA[m].empty) {
-          // ans
-          YESNO[true].writeln;
-          
-          auto ansArrayA = (okA[m].length - 1) ~ okA[m];
-          auto ansArrayB = (okA[k].length) ~ okA[k] ~ [i];
-          ansArrayA.map!"(a + 1).to!string".joiner(" ").writeln;
-          ansArrayB.map!"(a + 1).to!string".joiner(" ").writeln;
+    foreach(ref a; A) a %= 200;
+    const subSize = min(8, N);
 
-          return;
+    auto routes = new long[][](200, 0);
+    foreach(long i; 1..subSize + 1) {
+      foreach(il; iota(1, subSize + 1).array.combinations(i)) {
+        const mod = il.map!(i => A[i - 1]).sum % 200;
+        if (routes[mod].empty) {
+          routes[mod] = il.dup;
         } else {
-          okA[(k + a) % 200] = okA[k] ~ [i];
+          return [
+            YESNO[true],
+            (routes[mod].length ~ routes[mod]).toAnswerString,
+            (il.length ~ il).toAnswerString,
+          ];
         }
       }
     }
     
-    YESNO[false].writeln;
+    return [YESNO[false]];
   }
 
   outputForAtCoder(&solve);
@@ -35,7 +33,7 @@ void problem() {
 // ----------------------------------------------
 
 import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time;
-T[][] combinations(T)(T[] s, in int m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
 string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
 T scan(T)(){ return scan.to!T; }
 T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
@@ -46,9 +44,14 @@ long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n 
 bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
 bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
 string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").to!string; }
 void outputForAtCoder(T)(T delegate() fn) {
   static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
   else static if (is(T == void)) fn();
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
   else fn().writeln;
 }
 void runSolver() {
--- home/atcoder/0508_abc200/e.d
+++ home/atcoder/0508_abc200/e.d
@@ -5,9 +5,8 @@ void problem() {
   auto K = scan!long;
 
   auto solve() {
-    foreach(p; 1..N + 1) {
-
-    }
+    // dp[total score][max element score]
+    auto dp = new long[][](N*3 + 1, 1);
   }
 
   outputForAtCoder(&solve);
@@ -16,7 +15,7 @@ void problem() {
 // ----------------------------------------------
 
 import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time;
-T[][] combinations(T)(T[] s, in int m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
 string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
 T scan(T)(){ return scan.to!T; }
 T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
@@ -27,9 +26,14 @@ long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n 
 bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
 bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
 string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").to!string; }
 void outputForAtCoder(T)(T delegate() fn) {
   static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
   else static if (is(T == void)) fn();
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
   else fn().writeln;
 }
 void runSolver() {
