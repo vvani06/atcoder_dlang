24ffec2559b16407084002f7711f9a783448df4c
allegrogiken <vvani06@gmail.com>
2021-07-25 16:21:27 +0900

    abc211 solved: ABCD

---
+++ home/atcoder/0724_abc211/d.d
@@ -0,0 +1,83 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!long;
+  auto M = scan!long;
+  auto P = scan!long(2 * M).chunks(2);
+
+  auto solve() {
+    enum long MOD = 10L ^^ 9 + 7;
+    auto pathes = new long[][](N, 0);
+    foreach(p; P) {
+      p[0]--; p[1]--;
+      pathes[p[0]] ~= p[1];
+      pathes[p[1]] ~= p[0];
+    }
+
+    auto visited = new long[](N);
+    visited[0] = 1;
+    
+    auto from = new DList!long([0L]);
+    while(!from.empty) {
+      auto to = new long[](0);
+      long[long] add;
+
+      while(!from.empty) {
+        auto f = from.front;
+        from.removeFront;
+
+        foreach(n; pathes[f]) {
+          if (visited[n] > 0) continue;
+
+          add[n] += visited[f];
+          add[n] %= MOD;
+        }
+      }
+
+      foreach(k, v; add) {
+        visited[k] = v;
+        from.insertBack(k);
+      }
+    }
+
+    visited.deb;
+    return visited[$ - 1];
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(true) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
---
+++ home/atcoder/0724_abc211/e.d
@@ -0,0 +1,179 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!long;
+  auto K = scan!int;
+  auto M = scan!string(N).map!(s => s.map!(c => c == '.').array).array;
+
+  auto solve() {
+    auto base = new bool[](N * N);
+    bool[bool[]] ans;
+    auto rect = GridPoint(N, N);
+
+    foreach(y; 0..N) foreach(x; 0..N) {
+      if (!M[y][x]) continue;
+      
+      int size = 1;
+      for(auto q = new DList!GridPoint([GridPoint(x, y)]); !q.empty;) {
+        auto p = q.front;
+        q.removeFront;
+
+        foreach(a; p.around(rect)) {
+          if (!a.of(M)) continue;
+
+          size++;
+        }
+      }
+    }
+
+    return ans;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(true) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+
+struct GridPoint {
+  static enum ZERO = GridPoint(0, 0);
+  long x, y;
+ 
+  static GridPoint reversed(long y, long x) {
+    return GridPoint(x, y);
+  }
+ 
+  this(long x, long y) {
+    this.x = x;
+    this.y = y;
+  }
+ 
+  inout long toInt() { return y*16 + x; }
+  inout GridPoint left() { return GridPoint(x - 1, y); }
+  inout GridPoint right() { return GridPoint(x + 1, y); }
+  inout GridPoint up() { return GridPoint(x, y - 1); }
+  inout GridPoint down() { return GridPoint(x, y + 1); }
+  inout GridPoint leftUp() { return GridPoint(x - 1, y - 1); }
+  inout GridPoint leftDown() { return GridPoint(x - 1, y + 1); }
+  inout GridPoint rightUp() { return GridPoint(x + 1, y - 1); }
+  inout GridPoint rightDown() { return GridPoint(x + 1, y + 1); }
+  inout GridPoint[] around() { return [left(), up(), right(), down()]; }
+  inout GridPoint[] around(GridPoint max) { GridPoint[] ret; if (x > 0) ret ~= left; if(x < max.x-1) ret ~= right; if(y > 0) ret ~= up; if(y < max.y-1) ret ~= down; return ret; }
+  inout T of(T)(inout ref T[][] grid) { return grid[y][x]; }
+}
+ 
+struct GridValue(T) {
+  T nullValue;
+  GridPoint size;
+  T[][] g;
+ 
+  this(GridPoint p, T nullValue) {
+    size = p;
+    foreach(y; 0..size.y) {
+      g ~= new T[size.x];
+      g[$-1][] = nullValue;
+    }
+    this.nullValue = nullValue;
+  }
+ 
+  this(long width, long height, T nullValue) {
+    this(GridPoint(width, height), nullValue);
+  }
+ 
+  this(T[][] values, T nullValue) {
+    this.nullValue = nullValue;
+    size = GridPoint(values[0].length, values.length);
+    g = values;
+  }
+ 
+  bool contains(GridPoint p) { return (0 <= p.y && p.y < size.y && 0 <= p.x && p.x < size.x); }
+  T at(GridPoint p) { return contains(p) ? g[p.y][p.x] : nullValue; }
+  T opIndex(GridPoint p) { return at(p); }
+  T setAt(GridPoint p, T value) { return contains(p) ? g[p.y][p.x] = value : nullValue; }
+  T opIndexAssign(T value, GridPoint p) { return setAt(p, value); }
+}
+
+
+struct CombinationRange(T) {
+  private {
+    int combinationSize;
+    int elementSize;
+    int pointer;
+    int[] cursor;
+    T[] elements;
+    T[] current;
+  }
+
+  public:
+
+  this(T[] t, int combinationSize) {
+    this.combinationSize = combinationSize;
+    this.elementSize = cast(int)t.length;
+    pointer = combinationSize - 1;
+    cursor = new int[combinationSize];
+    current = new T[combinationSize];
+    elements = t.dup;
+    foreach(i; 0..combinationSize) {
+      cursor[i] = i;
+      current[i] = elements[i];
+    }
+  }
+
+  @property T[] front() {
+    return current;
+  }
+
+  void popFront() {
+    if (pointer == -1) return;
+
+    if (cursor[pointer] == elementSize + pointer - combinationSize) {
+      pointer--;
+      popFront();
+      if (pointer < 0) return;
+
+      pointer++;
+      cursor[pointer] = cursor[pointer - 1];
+      current[pointer] = elements[cursor[pointer]];
+    }
+
+    cursor[pointer]++;
+    current[pointer] = elements[cursor[pointer]];
+  }
+
+  bool empty() {
+    return pointer == -1;
+  }
+}
+CombinationRange!T combinations(T)(T[] t, int size) { return CombinationRange!T(t, size); }
