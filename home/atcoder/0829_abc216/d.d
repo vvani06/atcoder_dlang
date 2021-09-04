3d2dc36b6a054f87ba903bf9c369bf077ab6c41c
allegrogiken <vvani06@gmail.com>
2021-09-04 20:30:17 +0900

    ABC216
    - solved: ABCDE
    - upsolved: F

---
+++ home/atcoder/0829_abc216/d.d
@@ -0,0 +1,95 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!long;
+  auto M = scan!long;
+  long[][] P;
+  long[][] Pos = new long[][](N + 1, 0);
+  foreach(m; 0..M) {
+    const k = scan!long;
+    P ~= scan!long(k);
+    foreach(p; P[$ - 1]) Pos[p] ~= m;
+  }
+
+  Pos.deb;
+
+  auto solve() {
+    auto toDeletes = new DList!long();
+    {
+      long[long] pieces;
+      foreach(i, p; P) {
+        const head = p.front;
+        if (head in pieces && i != pieces[head]) {
+          toDeletes.insert(head);
+        } else {
+          pieces[head] = i;
+        }
+      }
+    }
+
+    if (toDeletes.empty) return YESNO[false];
+
+    long count;
+    auto indicies = new long[](M);
+    auto used = new bool[](N + 1);
+    while(!toDeletes.empty) {
+      const toDelete = toDeletes.front;
+      toDeletes.removeFront;
+      if (used[toDelete]) continue;
+
+      used[toDelete] = true;
+      count++;
+
+      foreach(pos; Pos[toDelete]) indicies[pos]++;
+      foreach(pos; Pos[toDelete]) {
+        if (P[pos].length == indicies[pos]) continue;
+
+        const next = P[pos][indicies[pos]];
+        const anotherPos = Pos[next][0] == pos ? Pos[next][1] : Pos[next][0];
+        if (next == P[anotherPos][indicies[anotherPos]]) toDeletes.insert(next);
+      }
+    }
+
+    return YESNO[count == N];
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
---
+++ home/atcoder/0829_abc216/e.d
@@ -0,0 +1,105 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!long;
+  auto K = scan!long;
+  auto A = scan!long(N).sort.reverse.array;
+ 
+  auto solve() {
+    long ans;
+    long pre = A[0];
+    long times;
+    long past = 1;
+    foreach(a; A[1..$] ~ 0) {
+      const t = past * (pre - a);
+      
+      if (times + t > K) {
+        const cycle = (K - times) / past;
+        [cycle, past, K, times].deb;
+        if (cycle > 0) {
+          const b = pre - cycle;
+          ans += past * ((pre + 1) * pre - (b + 1) * b) / 2;
+        }
+        [ans].deb;
+        const rest = K - times - cycle * past;
+        ans += (pre - cycle) * rest;
+        break;
+      } else {
+        ans += past * ((pre + 1) * pre - (a + 1) * a) / 2;
+        times += past * (pre - a);
+      }
+
+      pre = a;
+      past++;
+    }
+
+    return ans;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+T binarySearch(T, K)(K delegate(T) fn, bool delegate(K) cond, T l, T r) {
+  auto ok = l;
+  auto ng = r;
+  const T TWO = 2;
+ 
+  bool again() {
+    static if (is(T == float) || is(T == double) || is(T == real)) {
+      return !ng.approxEqual(ok, 1e-08, 1e-08);
+    } else {
+      return abs(ng - ok) > 1;
+    }
+  }
+ 
+  while(again()) {
+    const half = (ng + ok) / TWO;
+    const halfValue = fn(half);
+ 
+    if (cond(halfValue)) {
+      ok = half;
+    } else {
+      ng = half;
+    }
+  }
+ 
+  return ok;
+}
