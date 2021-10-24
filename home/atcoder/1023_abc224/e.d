89c5fa9521cbb5336b2373d0d7ebf171281727d7
vvani06 <vvani06+dev@gmail.co>
2021-10-24 08:43:24 +0000

    ABC224
    solved: ABCD
    upsolved: E
    WA/TLE: Cx1, Dx9

---
+++ home/atcoder/1023_abc224/e.d
@@ -0,0 +1,107 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto H = scan!long;
+  auto W = scan!long;
+  alias Node = Tuple!(long, "y", long, "x", long, "num", long, "index");
+  auto N = scan!long;
+  auto NODES = N.iota.map!(i => Node(scan!long - 1, scan!long - 1, scan!long, i)).array;
+
+  auto solve() {
+    enum compExp = "a.num == b.num ? a.index < b.index : a.num < b.num";
+    auto xs = H.iota.map!(_ => redBlackTree!compExp(new Node[](0))).array;
+    auto ys = W.iota.map!(_ => redBlackTree!compExp(new Node[](0))).array;
+    foreach(node; NODES) {
+      xs[node.y].insert(node);
+      ys[node.x].insert(node);
+    }
+    
+    long[Node] memo;
+    auto xMemo = new long[](H); xMemo[] = int.max;
+    auto xMemoV = new long[](H); 
+    auto yMemo = new long[](W); yMemo[] = int.max;
+    auto yMemoV = new long[](W);
+
+    auto comp = Node(0, 0, 0, int.max);
+    auto compX = Node(0, 0, 0, int.max);
+    auto compY = Node(0, 0, 0, int.max);
+    
+    long moves(Node node) {
+      if (node in memo) return memo[node];
+
+      const x = node.x;
+      const y = node.y;
+      comp.num = node.num;
+      long ret = max(xMemoV[y], yMemoV[x]);
+
+      long yMax;
+      foreach(nex; ys[x].upperBound(comp)) {
+        if (nex.num > yMemo[x]) break;
+        yMax.chmax(1 + moves(nex));
+      }
+      ret.chmax(yMax);
+      yMemoV[x].chmax(yMax);
+      yMemo[x].chmin(node.num);
+
+      long xMax;
+      foreach(nex; xs[y].upperBound(comp)) {
+        if (nex.num > xMemo[y]) break;
+        xMax.chmax(1 + moves(nex));
+      }
+      ret.chmax(xMax);
+      xMemoV[y].chmax(xMax);
+      xMemo[y].chmin(node.num);
+
+      memo[node] = ret;
+      return ret;
+    }
+
+    auto ans = new long[](N);
+    foreach(node; NODES.sort!"a.num > b.num") {
+      ans[node.index] = moves(node);
+    }
+
+    ans.each!writeln;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+struct CombinationRange(T) { private { int combinationSize; int elementSize; int pointer; int[] cursor; T[] elements; T[] current; } public: this(T[] t, int combinationSize) { this.combinationSize = combinationSize; this.elementSize = cast(int)t.length; pointer = combinationSize - 1; cursor = new int[combinationSize]; current = new T[combinationSize]; elements = t.dup; foreach(i; 0..combinationSize) { cursor[i] = i; current[i] = elements[i]; } } @property T[] front() { return current; } void popFront() { if (pointer == -1) return; if (cursor[pointer] == elementSize + pointer - combinationSize) { pointer--; popFront(); if (pointer < 0) return; pointer++; cursor[pointer] = cursor[pointer - 1]; current[pointer] = elements[cursor[pointer]]; } cursor[pointer]++; current[pointer] = elements[cursor[pointer]]; } bool empty() { return pointer == -1; } }
+CombinationRange!T combinations(T)(T[] t, int size) { return CombinationRange!T(t, size); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
