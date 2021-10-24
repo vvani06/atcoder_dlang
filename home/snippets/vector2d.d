01011d4d5e10d66b066a68c6ce74a62ac27f2658
vvani06 <vvani06+dev@gmail.co>
2021-10-24 08:43:37 +0000

    integer 2d vector

--- home/snippets/vector2d.d
+++ home/snippets/vector2d.d
@@ -12,3 +12,13 @@ struct Vector2 {
 
   string toString() { return "%.16f %.16f".format(x, y); }
 }
+
+struct Vector2 {
+  long x, y;
+  this(long x, long y) { this.x = x; this.y = y; }
+  Vector2 add(Vector2 other) { return Vector2(x + other.x, y + other.y ); }
+  Vector2 opAdd(Vector2 other) { return add(other); }
+  Vector2 sub(Vector2 other) { return Vector2(x - other.x, y - other.y ); }
+  Vector2 opSub(Vector2 other) { return sub(other); }
+  long dot(Vector2 other) {return x*other.y - y*other.x; }
+}
