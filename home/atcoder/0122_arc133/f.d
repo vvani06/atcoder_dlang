36475078b0bd1f5d9ce4731e61d757bcd82299d4
vvani06 <vvani06@gmail.com>
2022-01-23 11:52:37 +0900

    ARC133
    - solved: A, C

---
+++ home/atcoder/0122_arc133/f.d
@@ -0,0 +1,103 @@
+void main() { runSolver(); }
+
+enum long MOD = 998_244_353;
+enum long CHARS = 26;
+enum long MAX_SIZE = 5000;
+
+void problem() {
+  auto S = scan.map!(c => cast(int)(c - 'a')).array;
+  auto fermet = new FermetCalculator(MAX_SIZE);
+
+  auto solve() {
+    auto nums = new long[](CHARS);
+    foreach(long c; S) nums[c]++;
+    
+    auto dp = new long[][](CHARS + 1, MAX_SIZE + 1);
+    dp[0][0] = 1;
+    foreach(c; 0..CHARS) {
+      foreach(add; 0..nums[c] + 1) {
+        foreach(from; 0..MAX_SIZE - add + 1) {
+          const to = from + add;
+          dp[c + 1][to] += dp[c][from] * fermet.combine(to, add);
+          dp[c + 1][to] %= MOD;
+        }
+      }
+    }
+    
+    return dp[$ - 1][1..$].reduce!((r, a) => (r + a) % MOD);
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+Point invert(Point p) { return Point(p.y, p.x); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+class FermetCalculator {
+  long[] factrial; //階乗を保持
+  long[] inverse;  //逆元を保持
+  
+  this(long size) {
+    factrial = new long[size + 1];
+    inverse = new long[size + 1];
+    factrial[0] = 1;
+    inverse[0] = 1;
+    
+    for (long i = 1; i <= size; i++) {
+      factrial[i] = (factrial[i - 1] * i) % MOD;  //階乗を求める
+      inverse[i] = pow(factrial[i], MOD - 2) % MOD; // フェルマーの小定理で逆元を求める
+    }
+  }
+  
+  long combine(long n, long k) {
+    if (n < k) return 1;
+    return factrial[n] * inverse[k] % MOD * inverse[n - k] % MOD;
+  }
+  
+  long pow(long x, long n) { //x^n 計算量O(logn)
+    long ans = 1;
+    while (n > 0) {
+      if ((n & 1) == 1) {
+        ans = ans * x % MOD;
+      }
+      x = x * x % MOD; //一周する度にx, x^2, x^4, x^8となる
+      n >>= 1; //桁をずらす n = n >> 1
+    }
+    return ans;
+  }
+}
