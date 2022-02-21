226927dfdf099ac73edfc1c4aa3b31b0e7a673ab
allegrogiken <vvani06@gmail.com>
2022-02-21 22:41:52 +0900

    AHC008
    - first submit

--- home/heuristic/ahc008/src/a.d
+++ home/heuristic/ahc008/src/a.d
@@ -3,210 +3,112 @@ void main() { problem(); }
 // ----------------------------------------------
 
 void problem() {
-  enum long N = 20;
-  const SY = scan!long;
-  const SX = scan!long;
-  const H = scan!string(N).map!(s => s.map!(c => c == '1').array).array;
-  const W = scan!string(N - 1).map!(s => s.map!(c => c == '1').array).array;
+  enum int G = 30;
+  auto N = scan!int;
+  auto P = scan!int(3 * N).chunks(3).array;
+  auto M = scan!int;
+  auto H = scan!int(2 * M).chunks(2).array;
+  int[31][31] gp;
+  int[31][31] gh;
+  bool[31][31] proh;
 
-  bool isWall(GridPoint from, GridPoint to) {
-    if (from.x == to.x) {
-      return W[(from.y + to.y) / 2][from.x];
-    } else {
-      return H[from.y][(from.x + to.x) / 2];
+  class Pet {
+    int id, x, y, type;
+    this(int id, int x, int y, int type) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      this.type = type;
+      gp[y][x]++;
+    }
+
+    override string toString() {
+      return "Pet[%s:%s] (%s, %s)".format(id, type, x, y);
     }
   }
 
-  string toAns(int[] moves) {
-    string orders;
-    long dir = 1;
-    foreach(o; moves) {
-      if (dir == (o - 1 + 4) % 4) orders ~= 'R';
-      if (dir == (o + 1 + 4) % 4) orders ~= 'L';
-      if (dir == (o + 2 + 4) % 4) orders ~= "RR";
-      orders ~= "F";
-      dir = o;
+  class Human {
+    int id, x, y;
+    int gx, gy;
+    this(int id, int x, int y) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      gx = 1;
+      gy = max(5, y - y%5);
+      gh[y][x]++;
+    }
+    override string toString() {
+      return "Human[%s] (%s, %s)".format(id, x, y);
     }
 
-    foreach_reverse(times; 2..6) {
-      foreach_reverse(size; 3..17) {
-        orders = orders.replaceAll(
-          regex("(%s)\\1{%s}".format("[LRF]{%s}".format(size), times - 1)),
-          "%s($1)".format(times)
-        );
+    dchar move(int turn) {
+      gh[y][x]--;
+
+      dchar moveInner() {
+        if (x == gx) gx = gx == 1 ? 30 : 1;
+
+        if (y > gy) return 'L';
+        if (y < gy) return 'R';
+
+        if (turn < 8) return '.';
+        
+        const by = y - 1;
+        if (!proh[by][x]) {
+          if (gh[by][x] > 0) return '.';
+          if (gp[by][x] > 0) return '.';
+          foreach(xy; [[-1, 0], [0, -1], [1, 0], [0, 1]]) {
+            const ax = x + xy[0];
+            const ay = by + xy[1];
+            if (ax <= 0 || ay <= 0 || ax > G || ay > G) continue;
+
+            if (gp[ay][ax] > 0) return '.';
+          }
+
+          proh[by][x] = true;
+          return 'l';
+        }
+
+        if (x > gx) return 'U';
+        if (x < gx) return 'D';
+        return '.';
       }
+      auto ret = moveInner();
+      if (ret == 'L') y--;
+      if (ret == 'R') y++;
+      if (ret == 'U') x--;
+      if (ret == 'D') x++;
+
+      gh[y][x]++;
+      return ret;
     }
-
-    string compressed;
-    foreach(g; orders.group) {
-      if (g[1] == 1) {
-        compressed ~= g[0];
-        continue;
-      }
-
-      if (g[0] == 'L' || g[0] == 'F' || g[0] == 'R') {
-        compressed ~= g[1].to!string;
-        compressed ~= g[0];
-      } else {
-        compressed ~= g[0].repeat(g[1]).to!string;
-      }
-    }
-
-    return compressed;
   }
 
   auto solve() {
-    bool[N][N] visited;
-    alias Plot = Tuple!(long, "cost", GridPoint, "p", long, "dir");
+    auto pets = N.iota.map!(i => new Pet(i, P[i][0], P[i][1], P[i][2])).array;
+    auto humen = M.iota.map!(i => new Human(i, H[i][0], H[i][1])).array;
 
-    long delegate(Plot from, long dir, GridPoint to) calcCost;
-    calcCost = (Plot from, long dir, GridPoint to) => from.cost + 
-                       min((from.dir - dir).abs, (4 + from.dir - dir)) * 100 + 
-                       (visited[to.y][to.x] ? 100 : 0);
-
-    int[] route(GridPoint from, long fromDir, GridPoint to) {
-      Plot[N][N] plots;
-      foreach(ref pp; plots) foreach(ref p; pp) p.cost = int.max;
-      plots[from.y][from.x].cost = 0;
-      plots[from.y][from.x].cost = fromDir;
-
-      auto queue = [Plot(0, from, fromDir)].heapify!"a.cost > b.cost";
-      while(!queue.empty) {
-        auto f = queue.front; queue.removeFront;
-        auto p = f.p;
-        if (plots[p.y][p.x].cost < f.cost) continue;
-
-        // [[p], p.around].deb;
-        foreach(long dir, pa; p.around) {
-          if (pa.x < 0 || pa.y < 0 || pa.x >= N || pa.y >= N) continue;
-          if (memoize!isWall(p, pa)) continue;
-
-          const cost = calcCost(f, dir, pa);
-
-          if (plots[pa.y][pa.x].cost.chmin(cost)) {
-            plots[pa.y][pa.x].p = p;
-            plots[pa.y][pa.x].dir = f.dir;
-            // Plot(cost, pa, dir).deb;
-            if (pa == to) break;
-            queue.insert(Plot(cost, pa, dir));
-          }
-        }
-      }
-
-      auto tracer = to;
-      int[] trace;
-      while(tracer != from) {
-        // tracer.deb;
-        // plots[tracer.y][tracer.x].deb;
-        auto t = plots[tracer.y][tracer.x].p;
-        
-        if (tracer.x == t.x - 1) trace ~= 0;
-        if (tracer.y == t.y - 1) trace ~= 1;
-        if (tracer.x == t.x + 1) trace ~= 2;
-        if (tracer.y == t.y + 1) trace ~= 3;
-
-        visited[tracer.y][tracer.x] = true;
-        tracer = t;
-      }
-      visited[tracer.y][tracer.x] = true;
+    foreach(t; 0..300) {
+      // "turn: %s".format(t).deb;
+      humen.map!(h => h.move(t)).array.writeln;
       
-      return trace.reverse.array;
-    }
-
-    GridPoint[][] routes;
-    {
-      GridPoint[] ro;
-
-      ro.length = 0; foreach(y; 0..N) {
-        if (y % 2 == 0) foreach(x; 0..N) ro ~= GridPoint(x, y);
-        if (y % 2 == 1) foreach_reverse(x; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-      ro.length = 0; foreach(x; 0..N) {
-        if (x % 2 == 0) foreach(y; 0..N) ro ~= GridPoint(x, y);
-        if (x % 2 == 1) foreach_reverse(y; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-
-      ro.length = 0; foreach(y; 0..N) {
-        if (y % 2 == 1) foreach(x; 0..N) ro ~= GridPoint(x, y);
-        if (y % 2 == 0) foreach_reverse(x; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-      ro.length = 0; foreach(x; 0..N) {
-        if (x % 2 == 1) foreach(y; 0..N) ro ~= GridPoint(x, y);
-        if (x % 2 == 0) foreach_reverse(y; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-
-      ro.length = 0; foreach_reverse(y; 0..N) {
-        if (y % 2 == 0) foreach(x; 0..N) ro ~= GridPoint(x, y);
-        if (y % 2 == 1) foreach_reverse(x; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-      ro.length = 0; foreach_reverse(x; 0..N) {
-        if (x % 2 == 0) foreach(y; 0..N) ro ~= GridPoint(x, y);
-        if (x % 2 == 1) foreach_reverse(y; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-
-      ro.length = 0; foreach_reverse(y; 0..N) {
-        if (y % 2 == 1) foreach(x; 0..N) ro ~= GridPoint(x, y);
-        if (y % 2 == 0) foreach_reverse(x; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-      ro.length = 0; foreach_reverse(x; 0..N) {
-        if (x % 2 == 1) foreach(y; 0..N) ro ~= GridPoint(x, y);
-        if (x % 2 == 0) foreach_reverse(y; 0..N) ro ~= GridPoint(x, y);
-      } routes ~= ro;
-    }
-
-    long mi = int.max;
-    string ans;
-
-    foreach(ro; routes) {
-      int[] moves;
-      auto cur = GridPoint(SX, SY);
-      long dir = 1;
-
-      foreach(ref v; visited) v[] = false;
-      visited[SY][SX] = true;
-
-      foreach(to; ro) {
-        if (visited[to.y][to.x]) continue;
-
-        moves ~= route(cur, dir, to);
-        dir = moves[$ - 1];
-        cur = to;
+      stdout.flush();
+      foreach(ref p; pets) {
+        gp[p.y][p.x]--;
+        auto order = scan;
+        foreach(m; order) {
+          if (m == 'U') p.x--;
+          if (m == 'L') p.y--;
+          if (m == 'D') p.x++;
+          if (m == 'R') p.y++;
+        }
+        // p.deb;
+        gp[p.y][p.x]++;
       }
-
-      auto compressed = toAns(moves);
-      if (mi.chmin(compressed.length)) ans = compressed;
     }
-
-
-    calcCost = (Plot from, long dir, GridPoint to) => from.cost + 
-                      min((from.dir - dir).abs, (4 + from.dir - dir)) + 
-                      (visited[to.y][to.x] ? 1000 : 0);
-
-    foreach(ro; routes) {
-      int[] moves;
-      auto cur = GridPoint(SX, SY);
-      long dir = 1;
-
-      foreach(ref v; visited) v[] = false;
-      visited[SY][SX] = true;
-
-      foreach(to; ro) {
-        if (visited[to.y][to.x]) continue;
-
-        moves ~= route(cur, dir, to);
-        dir = moves[$ - 1];
-        cur = to;
-      }
-
-      auto compressed = toAns(moves);
-      if (mi.chmin(compressed.length)) ans = compressed;
-    }
-    
-    stderr.writeln(400 + (10L^^8) / (100L + ans.length));
-    return ans;
   }
 
-  solve().writeln;
+  solve();
 }
 
 // ----------------------------------------------
@@ -241,61 +143,3 @@ long distance(Point a, Point b) {
 }
 
 // -----------------------------------------------
-
-
-struct GridPoint {
-  static enum ZERO = GridPoint(0, 0);
-  long x, y;
- 
-  static GridPoint reversed(long y, long x) {
-    return GridPoint(x, y);
-  }
- 
-  this(long x, long y) {
-    this.x = x;
-    this.y = y;
-  }
- 
-  inout GridPoint left() { return GridPoint(x - 1, y); }
-  inout GridPoint right() { return GridPoint(x + 1, y); }
-  inout GridPoint up() { return GridPoint(x, y - 1); }
-  inout GridPoint down() { return GridPoint(x, y + 1); }
-  inout GridPoint leftUp() { return GridPoint(x - 1, y - 1); }
-  inout GridPoint leftDown() { return GridPoint(x - 1, y + 1); }
-  inout GridPoint rightUp() { return GridPoint(x + 1, y - 1); }
-  inout GridPoint rightDown() { return GridPoint(x + 1, y + 1); }
-  inout GridPoint[] around() { return [left(), up(), right(), down()]; }
-  inout GridPoint[] around(GridPoint max) { GridPoint[] ret; if (x > 0) ret ~= left; if(x < max.x-1) ret ~= right; if(y > 0) ret ~= up; if(y < max.y-1) ret ~= down; return ret; }
-  inout T of(T)(inout ref T[][] grid) { return grid[y][x]; }
-}
- 
-struct GridValue(T) {
-  T nullValue;
-  GridPoint size;
-  T[][] g;
- 
-  this(GridPoint p, T nullValue) {
-    size = p;
-    foreach(y; 0..size.y) {
-      g ~= new T[size.x];
-      g[$-1][] = nullValue;
-    }
-    this.nullValue = nullValue;
-  }
- 
-  this(long width, long height, T nullValue) {
-    this(GridPoint(width, height), nullValue);
-  }
- 
-  this(T[][] values, T nullValue) {
-    this.nullValue = nullValue;
-    size = GridPoint(values[0].length, values.length);
-    g = values;
-  }
- 
-  bool contains(GridPoint p) { return (0 <= p.y && p.y < size.y && 0 <= p.x && p.x < size.x); }
-  T at(GridPoint p) { return contains(p) ? g[p.y][p.x] : nullValue; }
-  T opIndex(GridPoint p) { return at(p); }
-  T setAt(GridPoint p, T value) { return contains(p) ? g[p.y][p.x] = value : nullValue; }
-  T opIndexAssign(T value, GridPoint p) { return setAt(p, value); }
-}
