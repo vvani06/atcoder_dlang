44d30059b38920ec42f74289538b27aae2fceb5a
vvani06 <vvani06@gmail.com>
2022-05-29 23:49:43 +0900

    ARC141
    - solved: AB
    - WA: Ax2, Bx4

---
+++ home/atcoder/0529_arc141/e.d
@@ -0,0 +1,74 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!long;
+  auto A = scan!long(N);
+  auto B = scan!long(N);
+  auto QN = scan!long;
+  auto Q = scan!long(2 * QN).chunks(2);
+  enum long MOD = 998_244_353;
+
+  auto solve() {
+    long[] a = new long[](N + 1);
+    long[] b = new long[](N + 1);
+    a[0] = b[0] = MOD - 1;
+    bool[long] va, vb;
+
+    foreach(i; 0..N) {
+      a[i + 1] = a[i];
+      b[i + 1] = b[i];
+      if (!(A[i] in va)) {
+        a[i + 1] = (a[i] * A[i]) % MOD;
+        va[A[i]] = true;
+      }
+      if (!(B[i] in vb)) {
+        b[i + 1] = (b[i] * B[i]) % MOD;
+        vb[B[i]] = true;
+      }
+    }
+    
+    foreach(q; Q) {
+      const x = q[0];
+      const y = q[1];
+
+      YESNO[a[x] == b[y]].writeln;
+    }
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
+T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+alias Point = Tuple!(long, "x", long, "y");
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
