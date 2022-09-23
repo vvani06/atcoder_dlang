23a812fd95a691c34578ca53e83bed1a6dc51619
vvani06 <vvani06@gmail.com>
2022-09-23 13:10:54 +0900

    ABC269
    - solved: ABCDE

---
+++ home/atcoder/0917_abc269/d.d
@@ -0,0 +1,108 @@
+void main() { runSolver(); }
+
+struct Coord {
+  int x, y;
+  int toInt() { return x + y * 2010; }
+
+  Coord[] around() {
+    return [
+      Coord(x - 1, y - 1),
+      Coord(x - 1, y),
+      Coord(x, y - 1),
+      Coord(x + 1, y + 1),
+      Coord(x + 1, y),
+      Coord(x, y + 1),
+    ];
+  }
+}
+
+void problem() {
+  auto N = scan!int;
+  auto P = scan!int(2 * N).chunks(2).map!(p => Coord(p[0] + 1002, p[1] + 1002)).array;
+
+  auto solve() {
+    auto grid = new bool[][](2010, 2010);
+    foreach(p; P) {
+      grid[p.x][p.y] = true;
+    }
+
+    auto uf = UnionFind(2010 * 2010);
+    foreach(p; P) {
+      foreach(a; p.around) {
+        if (grid[a.x][a.y]) uf.unite(a.toInt, p.toInt);
+      }
+    }
+
+    bool[int] roots;
+    foreach(p; P) roots[uf.root(p.toInt)] = true;
+    return roots.length;
+  }
+
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+struct UnionFind {
+  int[] parent;
+
+  this(int size) {
+    parent.length = size;
+    foreach(i; 0..size) parent[i] = i;
+  }
+
+  int root(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = root(parent[x]);
+  }
+
+  int unite(int x, int y) {
+    int rootX = root(x);
+    int rootY = root(y);
+
+    if (rootX == rootY) return rootY;
+    return parent[rootX] = rootY;
+  }
+
+  bool same(int x, int y) {
+    int rootX = root(x);
+    int rootY = root(y);
+
+    return rootX == rootY;
+  }
+}
