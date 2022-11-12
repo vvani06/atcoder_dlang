eae5714913d445eb7a9c480b0926cb9616bc698a
vvani06 <vvani06@gmail.com>
2022-11-12 18:21:31 +0900

    upsolved ABC276-F

--- home/atcoder/1105_abc276/f.d
+++ home/atcoder/1105_abc276/f.d
@@ -4,17 +4,44 @@ void problem() {
   auto N = scan!int;
   auto A = scan!int(N);
 
+  struct Item {
+    long number, count;
+    MInt9 sum;
+
+    this(long n, long c) {
+      number = n;
+      count = c;
+      sum = MInt9(n * c);
+    }
+
+    this(long n, long c, MInt9 s) {
+      number = n;
+      count = c;
+      sum = s;
+    }
+
+    Item add(Item other) {
+      return Item(0, count + other.count, sum + other.sum);
+    }
+
+    Item incr() {
+      return Item(number, count + 1);
+    }
+  }
+
   auto solve() {
-    foreach(k; 1..N + 1) {
-      auto arr = A[0..k];
+    const maxi = A.maxElement + 1;
+    auto items = maxi.iota.map!(i => Item(i, 0)).array;
+    auto segtree = SegTree!("a.add(b)", Item)(items, Item(0, 0));
 
-      MInt9 s;
-      foreach(i; 0..k) foreach(j; 0..k) {
-        s += MInt9(max(A[i], A[j]));
-      }
+    MInt9 ans;
+    foreach(i, a; A) {
+      ans += MInt9(a);
+      ans += MInt9(segtree.sum(0, a).count * 2 * a);
+      ans += segtree.sum(a, maxi).sum * MInt9(2);
+      writeln(ans / MInt9((i + 1)^^2));
 
-      s = s / MInt9(k * k);
-      s.writeln;
+      segtree.update(a, segtree.get(a).incr);
     }
   }
 
@@ -57,13 +84,50 @@ enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
 
-struct Vector2(T) {
-  T x, y;
-  Vector2 add(Vector2 other) { return Vector2(x + other.x, y + other.y ); }
-  Vector2 sub(Vector2 other) { return Vector2(x - other.x, y - other.y ); }
-  Vector2 opBinary(string op:"+")(Vector2 other) { return add(other); }
-  Vector2 opBinary(string op:"-")(Vector2 other) { return sub(other); }
-  T norm(Vector2 other) {return (x - other.x)*(x - other.x) + (y - other.y)*(y - other.y); }
-  T dot(Vector2 other) {return x*other.y - y*other.x; }
-  Vector2 normalize() {if (x == 0 || y == 0) return Vector2(x == 0 ? 0 : x/x.abs, y == 0 ? 0 : y/y.abs);const gcd = x.abs.gcd(y.abs);return Vector2(x / gcd, y / gcd);}
+struct SegTree(alias pred = "a + b", T = long) {
+  alias predFun = binaryFun!pred;
+  int size;
+  T[] data;
+  T monoid;
+ 
+  this(T[] src, T monoid = T.init) {
+    this.monoid = monoid;
+
+    for(int i = 2; i < 2L^^32; i *= 2) {
+      if (src.length <= i) {
+        size = i;
+        break;
+      }
+    }
+    
+    data = new T[](size * 2);
+    foreach(i, s; src) data[i + size] = s;
+    foreach_reverse(b; 1..size) {
+      data[b] = predFun(data[b * 2], data[b * 2 + 1]);
+    }
+  }
+ 
+  void update(int index, T value) {
+    int i = index + size;
+    data[i] = value;
+    while(i > 0) {
+      i /= 2;
+      data[i] = predFun(data[i * 2], data[i * 2 + 1]);
+    }
+  }
+ 
+  T get(int index) {
+    return data[index + size];
+  }
+ 
+  T sum(int a, int b, int k = 1, int l = 0, int r = -1) {
+    if (r < 0) r = size;
+    
+    if (r <= a || b <= l) return monoid;
+    if (a <= l && r <= b) return data[k];
+ 
+    T leftValue = sum(a, b, 2*k, l, (l + r) / 2);
+    T rightValue = sum(a, b, 2*k + 1, (l + r) / 2, r);
+    return predFun(leftValue, rightValue);
+  }
 }
