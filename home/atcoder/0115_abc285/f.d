33ffbd082ffa94fd822f986197b4a48d3dc12ddb
vvani06 <vvani06@gmail.com>
2023-01-19 20:47:13 +0900

    upsolved ABC285-F

--- home/atcoder/0115_abc285/f.d
+++ home/atcoder/0115_abc285/f.d
@@ -2,53 +2,57 @@ void main() { runSolver(); }
 
 void problem() {
   auto N = scan!int;
-  auto T = scan!string;
-
-  int[] zAlgorithm(string s) {
-    auto ret = new int[](s.length);
-    ret[0] = s.length.to!int;
-
-    int i = 1, j = 0;
-    while (i < s.length) {
-      while (i + j < s.length && s[j] == s[i + j]) j++;
-      ret[i] = j;
-      if (j == 0) {
-        i++;
-        continue;
-      }
-
-      int k = 1;
-      while (i + k < s.length && k + ret[k] < j) {
-        ret[i + k] = ret[k];
-        k++;
-      }
-
-      i += k;
-      j -= k;
-    }  
-
-    return ret;
-  }
+  auto S = scan!string.array;
+  auto QN = scan!int;
 
   auto solve() {
-    auto A = T[0..N];
-    auto B = T[N..$].array.reverse.to!string;
-    auto X = A ~ B;
-    auto Y = B ~ A;
-    auto zx = zAlgorithm(X) ~ 0;
-    auto zy = zAlgorithm(Y) ~ 0;
+    auto segtrees = new SegTree!("a + b", int)[](26);
+    foreach(i; 0..26) segtrees[i] = SegTree!("a + b", int)(new int[](N), 0);
+    foreach(i; 0..N) segtrees[S[i] - 'a'].update(i, 1);
 
-    [X, Y].deb;
-    [zx, zy].deb;
+    bool validate(int l, int r) {
+      auto counts = segtrees.map!(st => st.sum(l, r)).array;
 
-    foreach(i; 0..N + 1) {
-      if (zx[2 * N - i] == i && zy[N + i] == N - i) {
-        T[i..N + i].array.reverse.writeln;
-        return i;
+      // S[l, r] が昇順かどうか
+      int offset = l;
+      foreach(i, c; counts) {
+        if (segtrees[i].sum(offset, offset + c) != c) return false;
+        offset += c;
       }
+
+      // 昇順の文字列の頭と尻を除く部分が完全一致するかどうか
+      // aabbbcccdd が全体として、abbbcccd, aabbbcccd はOK, abbcccd などはNG とする
+      int mi = 27;
+      int ma = -1;
+      foreach(int i, c; counts) {
+        if (c > 0) {
+          mi = min(mi, i);
+          ma = max(ma, i);
+        }
+      }
+      foreach(i; mi + 1..ma) {
+        if (segtrees[i].sum(0, N) != counts[i]) return false;
+      }
+
+      return true;
     }
 
-    return -1;
+    foreach(_; 0..QN) {
+      const qt = scan!int;
+      if (qt == 1) {
+        const x = scan!int - 1;
+        const c = scan!dchar;
+
+        const pre = S[x];
+        S[x] = c;
+        segtrees[pre - 'a'].update(x, 0);
+        segtrees[c - 'a'].update(x, 1);
+      } else {
+        const l = scan!int - 1;
+        const r = scan!int;
+        writeln(YESNO[validate(l, r)]);
+      }
+    }
   }
 
   outputForAtCoder(&solve);
@@ -89,3 +93,70 @@ void runSolver() {
 enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
+
+struct SegTree(alias pred = "a + b", T = long) {
+  alias predFun = binaryFun!pred;
+  int size;
+  T[] data;
+  T monoid;
+ 
+  this(T[] src, T monoid = T.init) {
+    this.monoid = monoid;
+
+    for(int i = 2; i < 2L^^32; i *= 2) {
+      if (src.length <= i) {
+        size = i;
+        break;
+      }
+    }
+    
+    data = new T[](size * 2);
+    foreach(i, s; src) data[i + size] = s;
+    foreach_reverse(b; 1..size) {
+      data[b] = predFun(data[b * 2], data[b * 2 + 1]);
+    }
+  }
+ 
+  void update(int index, T value) {
+    int i = index + size;
+    data[i] = value;
+    while(i > 0) {
+      i /= 2;
+      data[i] = predFun(data[i * 2], data[i * 2 + 1]);
+    }
+  }
+ 
+  T get(int index) {
+    return data[index + size];
+  }
+ 
+  T sum(int a, int b, int k = 1, int l = 0, int r = -1) {
+    if (r < 0) r = size;
+    
+    if (r <= a || b <= l) return monoid;
+    if (a <= l && r <= b) return data[k];
+ 
+    T leftValue = sum(a, b, 2*k, l, (l + r) / 2);
+    T rightValue = sum(a, b, 2*k + 1, (l + r) / 2, r);
+    return predFun(leftValue, rightValue);
+  }
+}
+
+long countInvertions(T)(T[] arr) {
+  auto segtree = SegTree!("a + b", long)(new long[](arr.length));
+  long ret;
+  long pre = -1;
+  int[] adds;
+  foreach(a; arr.enumerate(0).array.sort!"a[1] > b[1]") {
+    auto i = a[0];
+    auto n = a[1];
+    if (pre != n) {
+      foreach(ai; adds) segtree.update(ai, segtree.get(ai) + 1);   
+      adds.length = 0;
+    }
+    adds ~= i;
+    pre = n;
+    ret += segtree.sum(0, i);
+  }
+  return ret;
+}
