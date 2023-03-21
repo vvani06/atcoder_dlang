27b92e422f99973ce0c615390dfb53dab4904244
vvani06 <vvani06@gmail.com>
2023-03-21 20:49:13 +0900

    特に良くなかったので戻す

--- home/heuristic/ahc019/src/a.d
+++ home/heuristic/ahc019/src/a.d
@@ -150,24 +150,22 @@ void problem() {
 
   auto solve() {
     auto state = State(D);
-    
-    while(true) {
-      Coord best1, best2;
+
+    foreach(x1, y1, z1; XYZ) {
+      auto from = Coord(x1, y1, z1);
+      Coord bestCoord;
       int best;
-      foreach(x1, y1, z1; XYZ) {
-        auto from = Coord(x1, y1, z1);
-        foreach(x2, y2, z2; XYZ) {
-          auto merged = state.merge(from, Coord(x2, y2, z2), true);
-          if (best.chmax(merged)) {
-            best1 = from;
-            best2 = Coord(x2, y2, z2);
-          }
+      foreach(x2, y2, z2; XYZ) {
+        auto merged = state.merge(from, Coord(x2, y2, z2), true);
+        if (best.chmax(merged)) {
+          bestCoord = Coord(x2, y2, z2);
         }
       }
-        
-      if (best == 0) break;
-      deb(best, ", ", [best1, best2]);
-      state.merge(best1, best2, false);
+      
+      if (best > 0) {
+        best.deb;
+        state.merge(from, bestCoord, false);
+      }
     }
 
     state.writeln;
