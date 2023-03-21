8be4e194624466ece2e93cec17a79bb5e86671d3
vvani06 <vvani06@gmail.com>
2023-03-21 14:44:28 +0900

    DFSでブロックをマージしてく実装

--- home/heuristic/ahc019/src/a.d
+++ home/heuristic/ahc019/src/a.d
@@ -2,8 +2,11 @@ void main() { runSolver(); }
 
 // ----------------------------------------------
 
+enum MAX_D = 14;
+alias MATRIX = int[MAX_D][MAX_D][MAX_D];
+// alias MATRIX = int[][][];
+
 void problem() {
-  enum MAX_D = 14;
   auto D = scan!int;
   auto Diota = D.iota;
   auto XYZ = cartesianProduct(Diota, Diota, Diota);
@@ -16,31 +19,98 @@ void problem() {
 
   struct Coord {
     int x, y, z;
-  }
 
-  struct Requirement {
-    bool[][][] front, right;
-
-    this(bool[][] f1, bool[][] r1, bool[][] f2, bool[][] r2) {
-      front ~= f1;
-      front ~= f2;
-      right ~= r1;
-      right ~= r2;
+    Coord add(Coord other) {
+      return Coord(x + other.x, y + other.y, z + other.z);
     }
+    Coord sub(Coord other) {
+      return Coord(x - other.x, y - other.y, z - other.z);
+    }
+
+    int min() { return std.algorithm.comparison.min(x, y, z); }
+    int max() { return std.algorithm.comparison.max(x, y, z); }
+    bool valid() { return min >= 0 && max < D; }
+
+    int of(MATRIX matrix) { return matrix[x][z][y]; }
+
+    enum AX = Coord(1, 0, 0);
+    enum AY = Coord(0, 1, 0);
+    enum AZ = Coord(0, 0, 1);
+    enum BX = Coord(-1, 0, 0);
+    enum BY = Coord(0, -1, 0);
+    enum BZ = Coord(0, 0, -1);
+    enum MOVES = [AX, AY, AZ, BX, BY, BZ];
   }
 
   struct State {
-   int[MAX_D][MAX_D][MAX_D][2] v;
+   MATRIX[2] v;
+   int[100000] size;
+   int vid;
+
+    void update(int i, Coord c, int value) {
+      v[i][c.x][c.z][c.y] = value;
+    }
 
     this(int d) {
+      // v = new int[][][][](2, d, d, d);
+
+      size[0] = 100000;
       foreach(i; 0..2) {
-        int blocks;
         foreach(x, y, z; XYZ) {
           if (!F[i][y][x] || !R[i][y][z]) continue;
 
-          v[i][x][z][y] = ++blocks;
+          v[i][x][z][y] = ++vid;
+          size[vid] = 1;
         }
       }
+    }
+
+    int merge(Coord from1, Coord from2, bool dryrun) {
+      if (size[from1.of(v[0])] != 1 || size[from2.of(v[1])] != 1) return 0;
+
+      MATRIX visited;
+      visited[from1.x][from1.z][from1.y] = 1;
+      const base = from1.of(v[0]);
+      if (!dryrun) update(1, from2, base);
+
+      int merged;
+      void dfs(Coord cur, Coord pre) {
+        if (cur.of(visited) || !cur.valid) return;
+        visited[cur.x][cur.z][cur.y] = 1;
+
+        auto diff = cur.sub(from1);
+        auto cur2 = from2.add(diff);
+        if (!cur2.valid) return;
+
+        // 体積1のブロックだけを侵食する
+        if (size[cur.of(v[0])] != 1) return;
+        if (size[cur2.of(v[1])] != 1) return;
+
+        // [cur, cur2].deb;
+        // [cur.of(v[0]), cur2.of(v[1])].deb;
+
+        // ブロックのマージ
+        merged++;
+        if (!dryrun) {
+          update(0, cur, base);
+          update(1, cur2, base);
+          size[base]++;
+        }
+
+        foreach(d; Coord.MOVES) {
+          auto next = cur.add(d);
+          if (next != pre && next.valid) {
+            dfs(next, cur);
+          }
+        }
+      }
+      
+      foreach(d; Coord.MOVES) {
+        auto next = from1.add(d);
+        if (next.valid) dfs(next, from1);
+      }
+
+      return merged;
     }
 
     string toString() {
@@ -69,7 +139,24 @@ void problem() {
 
   auto solve() {
     auto state = State(D);
-    
+
+    foreach(x1, y1, z1; XYZ) {
+      auto from = Coord(x1, y1, z1);
+      Coord bestCoord;
+      int best;
+      foreach(x2, y2, z2; XYZ) {
+        auto merged = state.merge(from, Coord(x2, y2, z2), true);
+        if (best.chmax(merged)) {
+          bestCoord = Coord(x2, y2, z2);
+        }
+      }
+      
+      if (best > 0) {
+        best.deb;
+        state.merge(from, bestCoord, false);
+      }
+    }
+
     state.writeln;
   }
 
