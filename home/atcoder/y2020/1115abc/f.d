77dc42251a5b002326e17dc8d5eb85fb71a29dd8
vvani06 <vvani06@gmail.com>
2023-07-29 20:26:45 +0900

    upsolved ABC183-F

--- home/atcoder/y2020/1115abc/f.d
+++ home/atcoder/y2020/1115abc/f.d
@@ -1,74 +1,124 @@
-void main() {
-  problem();
-}
-
-struct UnionFind(T) {
-  T[] parent;
-
-  this(T size) {
-    parent.length = size;
-    foreach(i; 0..size) parent[i] = i;
-  }
-
-  T root(T x) {
-    if (parent[x] == x) return x;
-    return parent[x] = root(parent[x]);
-  }
-
-  T unite(T x, T y) {
-    T rootX = root(x);
-    T rootY = root(y);
-
-    if (rootX == rootY) return rootY;
-    return parent[rootX] = rootY;
-  }
-
-  bool same(T x, T y) {
-    T rootX = root(x);
-    T rootY = root(y);
-
-    return rootX == rootY;
-  }
-}
+void main() { runSolver(); }
 
 void problem() {
-  const N = scan!long;
-  const M = scan!long;
-  auto F = M.iota.map!(_ => Point(scan!long - 1, scan!long - 1)).array;
+  auto N = scan!int;
+  auto QN = scan!int;
+  auto C = scan!int(N);
+  auto Q = scan!int(3 * QN).chunks(3);
 
-  void solve() {
-    auto unionFind = UnionFind!long(N);
-    auto friendsCount = new long[N];
-    auto candidates = new long[N];
-    candidates[] = 1;
-
-    foreach(f; F) {
-      const rootX = unionFind.root(f.x);
-      const united = unionFind.unite(f.x, f.y);
-      friendsCount[f.x]++;
-      friendsCount[f.y]++;
-
-      if (rootX != united) candidates[united] += candidates[rootX];
+  auto solve() {
+    auto uf = UnionFind(N, C);
+    foreach(q; Q) {
+      if (q[0] == 1) {
+        q[1]--;
+        q[2]--;
+        uf.unite(q[1], q[2]);
+      } else {
+        q[1]--;
+        auto g = uf.groups[uf.root(q[1])];
+        auto ans = g.get(q[2], 0);
+        ans.writeln;
+      }
     }
     
-    long ans = long.min;
-    foreach(i; 0..N) {
-      ans = max(ans, candidates[unionFind.root(i)]);
-    }
-    ans.writeln;
   }
 
-  solve();
+  outputForAtCoder(&solve);
 }
 
 // ----------------------------------------------
 
-import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric;
-T[][] combinations(T)(T[] s, in int m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+import std;
 string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
 T scan(T)(){ return scan.to!T; }
 T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
-void deb(T ...)(T t){ debug writeln(t); }
-alias Point = Tuple!(long, "x", long, "y");
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
+  }
+  return ret;
+}
+void deb(T ...)(T t){ debug asAnswer(t).writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
+
+struct UnionFind {
+  int[] parent;
+  int[] sizes;
+  int[int][] groups;
+ 
+  this(int size, int[] c) {
+    parent = size.iota.array;
+    sizes = 1.repeat(size).array;
+    groups = c.map!(i => [i: 1]).array;
+  }
+ 
+  int root(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = root(parent[x]);
+  }
+
+  int size(int x) {
+    return sizes[root(x)];
+  }
+ 
+  int unite(int x, int y) {
+    int rootX = root(x);
+    int rootY = root(y);
+ 
+    if (rootX == rootY) return rootY;
+
+    void mergeGroups(int from, int to) {
+      foreach(f, v; groups[from]) {
+        groups[to][f] += v;
+      }
+    }
+ 
+    if (groups[rootX].length < groups[rootY].length) {
+      sizes[rootY] += sizes[rootX];
+      mergeGroups(rootX, rootY);
+      return parent[rootX] = rootY;
+    } else {
+      sizes[rootX] += sizes[rootY];
+      mergeGroups(rootY, rootX);
+      return parent[rootY] = rootX;
+    }
+  }
+ 
+  bool same(int x, int y) {
+    int rootX = root(x);
+    int rootY = root(y);
+ 
+    return rootX == rootY;
+  }
+}
