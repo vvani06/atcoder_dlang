f1fedf9c15244154547c02b3ad93af513c060a48
vvani06 <vvani06@gmail.com>
2023-08-05 20:00:39 +0900

    upsolve ABC214-E

--- home/atcoder/y2021/0814_abc214/e.d
+++ home/atcoder/y2021/0814_abc214/e.d
@@ -1,43 +1,38 @@
 void main() { runSolver(); }
 
 void problem() {
-  auto H = scan!long;
-  auto W = scan!long;
-  auto M = cast(char[][])scan!string(H);
+  auto T = scan!int;
 
-  auto solve() {
-    enum int INF = int.max / 4;
-    auto dp = GridValue!int(W, H, INF);
-    auto start = GridPoint(0, 0);
- 
-    auto rect = GridPoint(W, H);
-    dp[start] = 0;
-    for(auto q = new DList!GridPoint([start]); !q.empty; ) {
-      auto p = q.front; q.removeFront;
-      const cost = dp[p];
-      
-      foreach(a; p.around(rect)) {
-        const c = a.of(M) == '#' ? 1 : 0;
-        if (c == 0) {
-          if (dp[a] > cost) {
-            dp[a] = cost;
-            q.insertFront(a);
-          }
-        } else {
-          foreach(aa; a.around8) {
-            if (!aa.isIn(rect)) continue;
+  auto subSolve(int N, int[][] LR) {
+    int[][int] rs; { foreach(lr; LR) rs[lr[0]] ~= lr[1]; }
+    auto ls = rs.keys.sort;
 
-            if (dp[aa] > cost + c) {
-              dp[aa] = cost + c;
-              q.insertBack(aa);
-            }
-          }
-        }
+    enum LOW = 1;
+    enum HIGH = 10^^9;
+    auto heap = new int[](0).heapify!"a > b";
+    for(int x = LOW; x <= HIGH;) {
+      if (x in rs) {
+        foreach(r; rs[x]) heap.insert(r);
+      }
+
+      if (heap.empty) {
+        auto next = ls.upperBound(x);
+        if (!next.empty) x = next.front; else break;
+      } else {
+        auto r = heap.front;
+        if (r >= x++) heap.removeFront; else break;
       }
     }
- 
-    auto goal = GridPoint(W - 1, H - 1);
-    return dp[goal];
+
+    return heap.empty;
+  }
+
+  auto solve() {
+    foreach(_; 0..T) {
+      auto N = scan!int;
+      auto ans = subSolve(N, scan!int(2 * N).chunks(2).array);
+      asAnswer(ans).writeln;
+    }
   }
 
   outputForAtCoder(&solve);
@@ -45,98 +40,46 @@ void problem() {
 
 // ----------------------------------------------
 
-import std.stdio, std.conv, std.array, std.string, std.algorithm, std.container, std.range, core.stdc.stdlib, std.math, std.typecons, std.numeric, std.traits, std.functional, std.bigint, std.datetime.stopwatch, core.time, core.bitop;
-T[][] combinations(T)(T[] s, in long m) {   if (!m) return [[]];   if (s.empty) return [];   return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m); }
+import std;
 string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
 T scan(T)(){ return scan.to!T; }
 T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
-void deb(T ...)(T t){ debug writeln(t); }
-alias Point = Tuple!(long, "x", long, "y");
-Point invert(Point p) { return Point(p.y, p.x); }
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
 long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
 bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
 bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
-string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
 ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
-string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
 struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
 alias MInt1 = ModInt!(10^^9 + 7);
 alias MInt9 = ModInt!(998_244_353);
-void outputForAtCoder(T)(T delegate() fn) {
-  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
-  else static if (is(T == void)) fn();
-  else static if (is(T == string)) fn().writeln;
-  else static if (isInputRange!T) {
-    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
-    else foreach(r; fn()) r.writeln;
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
   }
-  else fn().writeln;
+  return ret;
+}
+void deb(T ...)(T t){ debug asAnswer(t).writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
 }
 void runSolver() {
+  static import std.datetime.stopwatch;
   enum BORDER = "==================================";
-  debug { BORDER.writeln; while(true) { "<<< Process time: %s >>>".writefln(benchmark!problem(1)); BORDER.writeln; } }
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
   else problem();
 }
 enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
-
-
-struct GridPoint {
-  static enum ZERO = GridPoint(0, 0);
-  long x, y;
- 
-  static GridPoint reversed(long y, long x) {
-    return GridPoint(x, y);
-  }
- 
-  this(long x, long y) {
-    this.x = x;
-    this.y = y;
-  }
- 
-  inout GridPoint left() { return GridPoint(x - 1, y); }
-  inout GridPoint right() { return GridPoint(x + 1, y); }
-  inout GridPoint up() { return GridPoint(x, y - 1); }
-  inout GridPoint down() { return GridPoint(x, y + 1); }
-  inout GridPoint leftUp() { return GridPoint(x - 1, y - 1); }
-  inout GridPoint leftDown() { return GridPoint(x - 1, y + 1); }
-  inout GridPoint rightUp() { return GridPoint(x + 1, y - 1); }
-  inout GridPoint rightDown() { return GridPoint(x + 1, y + 1); }
-  inout GridPoint[] around() { return [left(), up(), right(), down()]; }
-  inout GridPoint[] around8() { return [left(), leftUp(), up(), rightUp(), right(), rightDown(), down(), leftDown()]; }
-  inout GridPoint[] around(GridPoint max) { GridPoint[] ret; if (x > 0) ret ~= left; if(x < max.x-1) ret ~= right; if(y > 0) ret ~= up; if(y < max.y-1) ret ~= down; return ret; }
-  inout bool isIn(GridPoint max) { return x >= 0 && y >= 0 && x < max.x && y < max.y; }
-  inout T of(T)(inout ref T[][] grid) { return grid[y][x]; }
-}
- 
-struct GridValue(T) {
-  T nullValue;
-  GridPoint size;
-  T[][] g;
- 
-  this(GridPoint p, T nullValue) {
-    size = p;
-    foreach(y; 0..size.y) {
-      g ~= new T[size.x];
-      g[$-1][] = nullValue;
-    }
-    this.nullValue = nullValue;
-  }
- 
-  this(long width, long height, T nullValue) {
-    this(GridPoint(width, height), nullValue);
-  }
- 
-  this(T[][] values, T nullValue) {
-    this.nullValue = nullValue;
-    size = GridPoint(values[0].length, values.length);
-    g = values;
-  }
- 
-  bool contains(GridPoint p) { return (0 <= p.y && p.y < size.y && 0 <= p.x && p.x < size.x); }
-  T at(GridPoint p) { return contains(p) ? g[p.y][p.x] : nullValue; }
-  T opIndex(GridPoint p) { return at(p); }
-  T setAt(GridPoint p, T value) { return contains(p) ? g[p.y][p.x] = value : nullValue; }
-  T opIndexAssign(T value, GridPoint p) { return setAt(p, value); }
-}
