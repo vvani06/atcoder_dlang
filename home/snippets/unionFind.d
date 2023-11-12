4fce8805773718119f640fa4cad4378a1e23f6bc
allegrogiken <vvani06+dev@gmail.com>
2023-11-12 17:12:57 +0900

    ABC328
    - solved: ABCD
    - upsolved: EF
    - UnionFind を重み付けに対応

--- home/snippets/unionFind.d
+++ home/snippets/unionFind.d
@@ -1,41 +1,48 @@
 
 struct UnionFind {
-  int[] parent;
+  int[] roots;
   int[] sizes;
+  long[] weights;
  
   this(int size) {
-    parent = size.iota.array;
+    roots = size.iota.array;
     sizes = 1.repeat(size).array;
+    weights = 0L.repeat(size).array;
   }
  
   int root(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = root(parent[x]);
+    if (roots[x] == x) return x;
+
+    const root = root(roots[x]);
+    weights[x] += weights[roots[x]];
+    return roots[x] = root;
   }
 
   int size(int x) {
     return sizes[root(x)];
   }
  
-  int unite(int x, int y) {
+  bool unite(int x, int y, long w = 0) {
     int rootX = root(x);
     int rootY = root(y);
- 
-    if (rootX == rootY) return rootY;
+    if (rootX == rootY) return weights[x] - weights[y] == w;
  
     if (sizes[rootX] < sizes[rootY]) {
-      sizes[rootY] += sizes[rootX];
-      return parent[rootX] = rootY;
-    } else {
-      sizes[rootX] += sizes[rootY];
-      return parent[rootY] = rootX;
+      swap(x, y);
+      swap(rootX, rootY);
+      w *= -1;
     }
+
+    sizes[rootX] += sizes[rootY];
+    weights[rootY] = weights[x] - weights[y] - w;
+    roots[rootY] = rootX;
+    return true;
   }
  
-  bool same(int x, int y) {
+  bool same(int x, int y, int w = 0) {
     int rootX = root(x);
     int rootY = root(y);
  
-    return rootX == rootY;
+    return rootX == rootY && weights[rootX] - weights[rootY] == w;
   }
 }
