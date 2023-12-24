2f82c82d143969327ad8fd299623967fbc5331cc
allegrogiken <vvani06+dev@gmail.com>
2023-12-24 22:39:36 +0900

    AHC027 last submit

--- home/heuristic/ahc027/src/a.d
+++ home/heuristic/ahc027/src/a.d
@@ -6,6 +6,7 @@ void problem() {
   int N = scan!int;
   int id(int y, int x) { return y + x * N; }
   
+  auto RND = Xorshift(0);
   auto StartTime = MonoTime.currTime();
   bool elapsed(int ms) { 
     return (ms <= (MonoTime.currTime() - StartTime).total!"msecs");
@@ -22,6 +23,8 @@ void problem() {
   enum RIGHT = 2;
   enum DOWN = 3;
   enum DIRS = "LURD";
+  enum DX = [-1, 0, 1, 0];
+  enum DY = [0, -1, 0, 1];
 
   struct Path {
     int to, dir;
@@ -177,6 +180,8 @@ void problem() {
       int rn = moves.map!"a.length.to!int".sum + lengthDelta;
       long newScore;
       foreach(i; 0..NN) {
+        if (newVisited[i].empty) return;
+
         int pret = newVisited[i].back - rn;
         foreach(t; newVisited[i].array ~ rn) {
           auto span = t - pret;
@@ -192,10 +197,69 @@ void problem() {
       
       newScore /= rn;
       if (score > newScore) {
-        [newScore, score].deb;
+        // [1, newScore, score].deb;
 
         route.insertInPlace(index, to);
-        moves = moves[0..index] ~ moves1 ~ moves2 ~ moves[index + 1..$];
+        moves.replaceInPlace(index, index + 1, [moves1, moves2]);
+        visited = newVisited;
+        score = newScore;
+      }
+    }
+
+    void remove(int index) {
+      int preLength = moves[0..index].map!"a.length.to!int".sum;
+      string moves1 = bfs(route[index - 1], route[index + 1])[0];
+      int lengthDelta = moves1.length.to!int - moves[index].length.to!int - moves[index + 1].length.to!int;
+      int removeLength = preLength + moves[index].length.to!int + moves[index + 1].length.to!int;
+
+      auto newVisited = NN.iota.map!(_ => new int[](0).redBlackTree).array;
+      foreach(i; 0..NN) {
+        foreach(t; visited[i]) {
+          if (t < preLength) {
+            newVisited[i].insert(t);
+          } else if (t < removeLength) {
+            // ignore entry
+          } else {
+            newVisited[i].insert(t + lengthDelta);
+          }
+        }
+      }
+
+      int pre = route[index - 1];
+      foreach(i, d; moves1) {
+        newVisited[pre].insert(preLength + i.to!int);
+        
+        if (d == DIRS[LEFT]) pre-=N;
+        if (d == DIRS[RIGHT]) pre+=N;
+        if (d == DIRS[UP]) pre--;
+        if (d == DIRS[DOWN]) pre++;
+      }
+
+      // newVisited.deb;
+      int rn = moves.map!"a.length.to!int".sum + lengthDelta;
+      long newScore;
+      foreach(i; 0..NN) {
+        if (newVisited[i].empty) return;
+
+        int pret = newVisited[i].back - rn;
+        foreach(t; newVisited[i].array ~ rn) {
+          auto span = t - pret;
+          if (pret < 0) {
+            newScore += D[i] * - pret * t;
+            newScore += D[i] * t * (t - 1) / 2;
+          } else {
+            newScore += D[i] * span * (span - 1) / 2;
+          }
+          pret = t;
+        }
+      }
+      
+      newScore /= rn;
+      if (score > newScore) {
+        // [-1, newScore, score].deb;
+
+        route.replaceInPlace(index, index + 1, new int[](0));
+        moves.replaceInPlace(index, index + 2, [moves1]);
         visited = newVisited;
         score = newScore;
       }
@@ -214,36 +278,115 @@ void problem() {
       }
 
       auto ranked = D.enumerate(0).array.sort!"a[1] > b[1]";
-      auto toVisits = ranked[0..$ / (N / 3)].map!"a[0]".redBlackTree;
-      auto visited = new bool[](NN);
+      auto toVisits = ranked[0..$ / 4].map!"a[0]".redBlackTree;
       while(!toVisits.empty) {
         auto route = bfsSet(pre, toVisits);
         initialRoute ~= pre = route[1];
         toVisits.removeKey(pre);
-        foreach(t; toVisits.array) {
-          if (visited[t]) toVisits.removeKey(t);
-        }
       }
       initialRoute ~= 0;
     }
+    int[] initialRoute2; {
+      int pre;
+      foreach(x; 0..N) foreach(y; x % 2 == 0 ? N.iota.array : N.iota.retro.array) {
+        initialRoute2 ~= pre = id(y, x);
+      }
+
+      auto ranked = D.enumerate(0).array.sort!"a[1] > b[1]";
+      auto toVisits = ranked[0..$ / 4].map!"a[0]".redBlackTree;
+      while(!toVisits.empty) {
+        auto route = bfsSet(pre, toVisits);
+        initialRoute2 ~= pre = route[1];
+        toVisits.removeKey(pre);
+      }
+      initialRoute2 ~= 0;
+    }
+    int[] initialRoute3; {
+      int pre;
+      {
+        int preY, preX, dir;
+        auto visited = new bool[](NN);
+        visited[0] = true;
+        int visitedCount = 1;
+        while(visitedCount < NN) {
+          while(true) {
+            auto y = preY + DY[dir];
+            auto x = preX + DX[dir];
+            if (min(x, y) >= 0 && max(x, y) < N && !visited[id(y, x)]) break;
+
+            dir = (dir + 1) % 4;
+          }
+
+          preY = preY + DY[dir];
+          preX = preX + DX[dir];
+          initialRoute3 ~= id(preY, preX);
+          visited[id(preY, preX)] = true;
+          visitedCount++;
+        }
+      }
+
+      auto ranked = D.enumerate(0).array.sort!"a[1] > b[1]";
+      auto toVisits = ranked[0..$ / 4].map!"a[0]".redBlackTree;
+      while(!toVisits.empty) {
+        auto route = bfsSet(pre, toVisits);
+        initialRoute3 ~= pre = route[1];
+        toVisits.removeKey(pre);
+      }
+      initialRoute3 ~= 0;
+    }
+    int[] initialRoute4; {
+      int pre;
+      {
+        int preY, preX, dir;
+        auto visited = new bool[](NN);
+        visited[0] = true;
+        int visitedCount = 1;
+        while(visitedCount < NN) {
+          while(true) {
+            auto y = preY + DY[dir];
+            auto x = preX + DX[dir];
+            if (min(x, y) >= 0 && max(x, y) < N && !visited[id(y, x)]) break;
+
+            dir = (dir + 3) % 4;
+          }
+
+          preY = preY + DY[dir];
+          preX = preX + DX[dir];
+          initialRoute4 ~= id(preY, preX);
+          visited[id(preY, preX)] = true;
+          visitedCount++;
+        }
+      }
+
+      auto ranked = D.enumerate(0).array.sort!"a[1] > b[1]";
+      auto toVisits = ranked[0..$ / 4].map!"a[0]".redBlackTree;
+      while(!toVisits.empty) {
+        auto route = bfsSet(pre, toVisits);
+        initialRoute4 ~= pre = route[1];
+        toVisits.removeKey(pre);
+      }
+      initialRoute4 ~= 0;
+    }
 
     auto ranked = D.enumerate(0).array.sort!"a[1] > b[1]";
-    auto toVisits = ranked[0..$ / (N / 3)].map!"a[0]".array;
-    auto bestRoute = Route(initialRoute);
+    auto toVisits = ranked[0..$ / 5].map!"a[0]".array;
+    // auto bestRoute = min(Route(initialRoute), Route(initialRoute2), Route(initialRoute3));
+    auto bestRoute = min(Route(initialRoute3), Route(initialRoute4));
 
     int tried;
-    while(!elapsed(1800)) {
-      auto index = uniform(1, bestRoute.route.length.to!int);
-      auto toVisit = toVisits.choice();
-
-      auto newRouteArray = bestRoute.route.dup;
-      newRouteArray.insertInPlace(index, toVisit);
+    while(!elapsed(8000)) {
+      auto index = uniform(1, bestRoute.route.length.to!int, RND);
+      auto toVisit = toVisits.choice(RND);
       bestRoute.addTo(index, toVisit);
+
+      auto removeIndex = uniform(1, bestRoute.route.length.to!int - 1, RND);
+      bestRoute.remove(removeIndex);
       tried++;
     }
     
     [[tried]].deb;
     auto ans = bestRoute.toAns();
+    ans[1].deb;
     ans[0].writeln;
   }
 
