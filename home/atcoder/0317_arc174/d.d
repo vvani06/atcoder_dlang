a189d637c9bb34c60e42559d91b432bd9e57cf99
vvani06 <vvani06@gmail.com>
2024-03-23 13:27:35 +0900

    ARC174
    - solved: ABD
    - WA: Ax1, Bx1, Dx1
      - A: C=0の時のハンドリングが甘かった
      - B: 入力にある星4, 5のレビューを無視しちゃってた 愚か
      - C: 平方が10^^8までしか計算してなかった 愚か

---
+++ home/atcoder/0317_arc174/d.d
@@ -0,0 +1,88 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto T = scan!int;
+
+  struct Tester {
+    long root, sqMin, sqMax;
+
+    long count(long n) {
+      if (n < sqMin) return 0;
+
+      [min(n, sqMax), sqMin, min(n, sqMax) - sqMin + 1].deb;
+      return min(n, sqMax) - sqMin + 1;
+    }
+  }
+
+  Tester[] testers;
+  foreach(i; 1L..10L) {
+    const base = 10L ^^ i;
+    const square = 10L ^^ (2 * i);
+
+    testers ~= Tester(base - 2, (base - 2) * base, (base - 2) * base);
+    testers ~= Tester(base - 1, (base - 1) * base, square - 1);
+    testers ~= Tester(base, square, square + base - 1);
+  }
+  
+  auto subSolve(long N) {
+    long ans = 1;
+    foreach(t; testers) {
+      ans += t.count(N);
+    }
+    return ans;
+  }
+
+  auto subSolveNaive(long N) {
+    long ans;
+    foreach(x; 1..N + 1) {
+      auto sqx = x.to!real.sqrt.to!long;
+      if (x.to!string.startsWith(sqx.to!string)) {
+        [x, sqx].deb;
+        ans++;
+      }
+    }
+    return ans;
+  }
+
+  auto solve() {
+    foreach(_; 0..T) subSolve(scan!long).writeln;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(T t){ debug writeln(t); }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
