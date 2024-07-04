baddce8b7d306111130c0b4ef63f529a21f3387d
allegrogiken <vvani06@gmail.com>
2024-07-04 23:27:27 +0900

    ABC360
    - solved: ABCDE

---
+++ home/atcoder/0630_abc360/g.d
@@ -0,0 +1,175 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto S = scan;
+  auto M = scan!int;
+  auto T = scan!string(M);
+
+  struct RollingHash {
+    ulong[] hashes = [0];
+    ulong[] powers = [1];
+
+    enum ulong MOD = 2L^^61 - 1;
+    static uint seed;
+    static ulong base;
+
+    static this() {
+      base = uniform(2, MOD - 1);
+      seed = unpredictableSeed;
+    }
+
+    ulong mul(ulong a, ulong b) {
+      Int128 c = a;
+      c *= b;
+      c %= MOD;
+      return c.data.lo;
+    }
+
+    this(T)(T[] arr) {
+      foreach(i, a; arr) {
+        hashes ~= (mul(hashes[i], base) + a.hashOf(seed)) % MOD;
+        powers ~= mul(powers[i], base) % MOD;
+      }
+    }
+
+    ulong get(size_t l, size_t r) {
+      return (MOD + hashes[r] - mul(powers[r - l], hashes[l])) % MOD;
+    }
+  }
+
+  auto solve() {
+    auto targets = new ulong[][](11, 0).map!redBlackTree.array;
+    foreach(t; T) {
+      auto rh = RollingHash(t);
+      targets[t.length].insert(rh.get(0, t.length));
+    }
+
+    long ans;
+    auto rh = RollingHash(S);
+    foreach(l; 1..11) {
+      targets[l].deb;
+
+      foreach(i; 0..S.length.to!long - l) {
+        auto hash = rh.get(i, i + l);
+        if (hash in targets[l]) ans++; 
+      }
+    }
+
+    return ans;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+import core.bitop;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
+  }
+  return ret;
+}
+void deb(T ...)(T t){ debug t.writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+struct SegTree(alias pred = "a + b", T = long) {
+  alias predFun = binaryFun!pred;
+  int size;
+  T[] data;
+  T monoid;
+ 
+  this(T[] src, T monoid = T.init) {
+    this.monoid = monoid;
+
+    for(int i = 2; i < 2L^^32; i *= 2) {
+      if (src.length <= i) {
+        size = i;
+        break;
+      }
+    }
+    
+    data = new T[](size * 2);
+    foreach(i, s; src) data[i + size] = s;
+    foreach_reverse(b; 1..size) {
+      data[b] = predFun(data[b * 2], data[b * 2 + 1]);
+    }
+  }
+ 
+  void update(int index, T value) {
+    int i = index + size;
+    data[i] = value;
+    while(i > 0) {
+      i /= 2;
+      data[i] = predFun(data[i * 2], data[i * 2 + 1]);
+    }
+  }
+ 
+  T get(int index) {
+    return data[index + size];
+  }
+ 
+  T sum(int a, int b, int k = 1, int l = 0, int r = -1) {
+    if (r < 0) r = size;
+    
+    if (r <= a || b <= l) return monoid;
+    if (a <= l && r <= b) return data[k];
+ 
+    T leftValue = sum(a, b, 2*k, l, (l + r) / 2);
+    T rightValue = sum(a, b, 2*k + 1, (l + r) / 2, r);
+    return predFun(leftValue, rightValue);
+  }
+}
+
+struct ModInt(ulong MD) if (MD < long.max) {
+  ulong v;
+  this(string v) {this(v.to!long);}
+  this(int v) {this(long(v));}
+  this(long v) {this.v = (v%MD+MD)%MD;}
+  void opAssign(ulong t) {v = (t%MD+MD)%MD;}
+  static auto normS(ulong x) {return (x<MD)?x:x-MD;}
+  static auto make(ulong x) {ModInt m; m.v = x; return m;}
+  auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}
+  auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}
+  auto opBinary(string op:"*")(ModInt r) const {return make(v*r.v%MD);}
+  auto opBinary(string op:"^^")(ulong r) const {ulong x=v;ulong y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}
+  auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}
+  static ModInt inv(ModInt x) {return x^^(MD-2);}
+  string toString() const {return v.to!string;}
+  auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}
+}
+
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
