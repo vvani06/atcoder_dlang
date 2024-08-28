72161751e9bf5cb74f1f748585a94afec0daa531
allegrogiken <vvani06+dev@gmail.com>
2024-08-29 00:47:18 +0900

    AHC036 重心を2つ以上にしてみる実験実装

--- home/heuristic/ahc036/src/a.d
+++ home/heuristic/ahc036/src/a.d
@@ -87,6 +87,7 @@ void problem() {
       nodeCosts = costs.dup;
 
       provisionRoute();
+      // provisionSignal2();
 
       if (LA * (24 - LB) >= 14_000) {
         provisionSignal2();
@@ -355,17 +356,48 @@ void problem() {
   long[] costsNormal = 1L.repeat(N).array;
   long[][] allCosts = calcDistances(graphNormal, costsNormal);
   int[][] graphMST2 = new int[][](N, 0); {
+
+    int left = allCosts[0].maxIndex.to!int;
+    long maxDistance = allCosts[left].maxElement;
+
+    auto tTree = T.redBlackTree!true;
+    auto nodes = N.iota.redBlackTree;
+    int[] centers;
+    
     long bestDistSum = long.max;
     int center;
-    foreach(n; 0..N) {
+    long border = maxDistance / 6;
+    foreach(_; 0..10) {
+      long bestCount;
+      int bestNode;
+      int[] tos = tTree.dup.array;
+      if (tos.length < 100) break;
+      
+      foreach(f; nodes.dup) {
+        if (bestCount.chmax(tos.count!(t => allCosts[f][t] <= border))) bestNode = f;
+      }
+
+      foreach(t; tos) {
+        if (allCosts[bestNode][t] <= border) {
+          tTree.removeKey(t);
+          nodes.removeKey(t);
+        }
+      }
+      centers ~= bestNode;
+    }
+
+    foreach(n; tTree) {
       if (bestDistSum.chmin(T.map!(t => allCosts[n][t]).sum)) {
         center = n;
       }
     }
 
+    centers ~= center;
+    centers.deb;
+
     UnionFind uf = UnionFind(N);
     bool[] visited = new bool[](N);
-    for(auto queue = DList!int(center); !queue.empty;) {
+    for(auto queue = DList!int([center]); !queue.empty;) {
       auto cur = queue.front();
       queue.removeFront();
       if (visited[cur]) continue;
