b7b9161b088f351ffac0c4daa7aeb7c7e38dd1b7
allegrogiken <vvani06+dev@gmail.com>
2024-09-15 21:32:29 +0900

    AHC037 親を同じくする2点を選んで、中継点を作る

--- home/heuristic/ahc037/src/a.d
+++ home/heuristic/ahc037/src/a.d
@@ -24,13 +24,20 @@ void problem() {
       return a + b;
     }
 
-    long dist(Soda other) {
+    Soda viaPoint(Soda other) {
+      return Soda(
+        min(a, other.a),
+        min(b, other.b),
+      );
+    }
+
+    long dist(ref Soda other) {
       if (other.a < a || other.b < b) return long.max;
 
       return other.a - a + other.b - b;
     }
 
-    inout opCmp(Soda other) {
+    inout opCmp(ref Soda other) {
       return cmp(
         [a, b],
         [other.a, other.b]
@@ -38,43 +45,123 @@ void problem() {
     }
   }
 
+  struct Eval {
+    int node;
+    long cost;
+
+    inout opCmp(inout const Eval other) {
+      return cmp(
+        [cost, node],
+        [other.cost, other.node],
+      );
+    }
+  }
+
   Soda[] requirements = AB.map!(ab => Soda(ab[0], ab[1])).array;
   Soda[] nodes = [Soda(0, 0)] ~ requirements.sort!"a.sum < b.sum".array;
+  int[Soda] nodeIndexPerSoda = [Soda(0, 0): 0];
   int[][] graph = new int[][](N ^^ 2, 0);
+  long[] costs = new long[](N ^^ 2);
+  auto evaluates = new Eval[](0).redBlackTree;
   
   foreach(to, req; nodes[1..$].enumerate(1)) {
-    long best = long.max;
-    long fromBest;
+    long distBest = long.max;
+    int fromBest;
     foreach(i, from; nodes) {
       if (to == i) continue;
-      if (best.chmin(from.dist(req))) fromBest = i;
+      if (distBest.chmin(from.dist(req))) fromBest = i.to!int;
     }
     
     graph[fromBest] ~= to;
+    nodeIndexPerSoda[req] = to;
+    auto eval = Eval(fromBest, costs[fromBest]);
+    evaluates.removeKey(eval);
+    costs[fromBest] += distBest;
+    eval.cost += distBest;
+    evaluates.insert(eval);
   }
 
-  struct CreateSoda {
-    Soda from;
-    Soda to;
+  long calcCost(int from, int to) {
+    return min(
+      nodes[from].dist(nodes[to]),
+      nodes[to].dist(nodes[from]),
+    );
+  }
 
-    string toOutput() {
-      return "%s %s %s %s".format(from.a, from.b, to.a, to.b);
+  int newNodeIndex = N + 1;
+  foreach(_; 0..4 * N) {
+    if (evaluates.empty) break;
+    auto worst = evaluates.back;
+    evaluates.removeBack;
+    auto from = worst.node;
+    auto nextSize = graph[from].length.to!int;
+
+    long bestImprove = 0;
+    int[] bestPair;
+    foreach(i; 0..nextSize - 1) {
+      auto destA = graph[from][i];
+      long baseCost = calcCost(from, destA);
+      foreach(j; i + 1..nextSize) {
+        auto destB = graph[from][j];
+        long cost = baseCost + calcCost(from, destB);
+
+        auto via = nodes[destA].viaPoint(nodes[destB]);
+        long viaCost = via.dist(nodes[destA]) + via.dist(nodes[destB]) + nodes[from].dist(via);
+        long improve = cost - viaCost;
+
+        if (bestImprove.chmax(improve)) {
+          bestPair = [i, j];
+        }
+      }
     }
-  }
 
-  CreateSoda[] ans;
-  for(auto queue = DList!int(0); !queue.empty;) {
-    auto cur = queue.front;
-    queue.removeFront;
+    if (bestPair.empty) continue;
 
-    foreach(next; graph[cur]) {
-      ans ~= CreateSoda(nodes[cur], nodes[next]);
-      queue.insertBack(next);
+    // deb(bestImprove, bestPair);
+    worst.cost -= bestImprove;
+    evaluates.insert(worst);
+    auto destA = graph[from][bestPair[0]];
+    auto destB = graph[from][bestPair[1]];
+    graph[from] = graph[from][0..bestPair[1]] ~ graph[from][bestPair[1] + 1..$];
+    graph[from] = graph[from][0..bestPair[0]] ~ graph[from][bestPair[0] + 1..$];
+
+    auto via = nodes[destA].viaPoint(nodes[destB]);
+    int newIndex;
+    if (via in nodeIndexPerSoda) {
+      newIndex = nodeIndexPerSoda[via];
+    } else {
+      newIndex = newNodeIndex++;
     }
+    if (!graph[from].canFind(newIndex)) graph[from] ~= newIndex;
+    graph[newIndex] ~= [destA, destB];
+    nodes ~= via;
   }
 
-  writeln(ans.length);
-  foreach(a; ans) writeln(a.toOutput());
+  { // output --------------------------------------------------------
+    struct CreateSoda {
+      Soda from;
+      Soda to;
+
+      string toOutput() {
+        return "%s %s %s %s".format(from.a, from.b, to.a, to.b);
+      }
+    }
+
+    CreateSoda[] ans;
+    for(auto queue = DList!int(0); !queue.empty;) {
+      auto cur = queue.front;
+      queue.removeFront;
+
+      foreach(next; graph[cur]) {
+        ans ~= CreateSoda(nodes[cur], nodes[next]);
+        queue.insertBack(next);
+      }
+    }
+
+    writeln(ans.length);
+    foreach(a; ans) writeln(a.toOutput());
+  }
+
 }
 
 // ----------------------------------------------
