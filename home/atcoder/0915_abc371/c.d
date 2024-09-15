81852e174b7ff2a93d2dbf5fe86b10e2615752a4
allegrogiken <vvani06+dev@gmail.com>
2024-09-15 16:19:54 +0900

    ABC371
    - solved: ABCDE

---
+++ home/atcoder/0915_abc371/c.d
@@ -0,0 +1,93 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!int;
+  auto MG = scan!int;
+  auto EG = scan!int(2 * MG).chunks(2);
+  auto MH = scan!int;
+  auto EH = scan!int(2 * MH).chunks(2);
+  auto A = scan!int(N * (N - 1) / 2);
+
+  auto solve() {
+    int[][] costs = new int[][](N, 0);
+    foreach(i; 0..N - 1) {
+      costs[i] = A[0..N - i - 1].dup;
+      A = A[N - i - 1..$];
+    }
+    
+    bool[][] g = new bool[][](N, N);
+    foreach(e; EG) {
+      e[0]--; e[1]--;
+      g[e[0]][e[1]] = g[e[1]][e[0]] = true;
+    }
+    
+    bool[][] h = new bool[][](N, N);
+    foreach(e; EH) {
+      e[0]--; e[1]--;
+      h[e[0]][e[1]] = h[e[1]][e[0]] = true;
+    }
+
+    long best = long.max;
+    foreach(perm; N.iota.permutations) {
+      long ans;
+      foreach(i; 0..N - 1) foreach(j; i + 1..N) {
+        auto f = perm[i];
+        auto t = perm[j];
+        if (g[f][t] != h[i][j]) {
+          ans += costs[i][j - i - 1];
+        }
+      }
+      best = min(best, ans);
+    }
+    return best;
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+import core.bitop;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
+  }
+  return ret;
+}
+void deb(T ...)(T t){ debug t.writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
