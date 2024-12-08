b11251f10e4e4c04635953c661933d1dfe5376f7
allegrogiken <vvani06+dev@gmail.com>
2024-12-08 17:49:36 +0900

    AHC040 戻し

--- home/heuristic/ahc040/src/a.d
+++ home/heuristic/ahc040/src/a.d
@@ -13,16 +13,16 @@ void problem() {
   int T = scan!int;
   int S = scan!int;
   long[][] WH = scan!long(N * 2).chunks(2).array;
+
   foreach(ref wh; WH) {
     wh[0] = clamp(wh[0], 10^^4, 10^^5);
     wh[1] = clamp(wh[1], 10^^4, 10^^5);
   }
 
-  long baseHeight = WH.map!"a[0] * a[1]".sum.to!real.sqrt.to!long;
   // baseHeight.deb;
 
   int rc;
-  while (T > N + 1) {
+  while (T > N + 50) {
     rc++;
     foreach(i; 0..N) {
       writeln(1);
@@ -37,6 +37,7 @@ void problem() {
     WH[i][0] /= rc + 1;
     WH[i][1] /= rc + 1;
   }
+  long baseHeight = WH.map!"a[0] * a[1]".sum.to!real.sqrt.to!long;
 
   class Rect {
     int rectId;
@@ -49,6 +50,12 @@ void problem() {
       baseRectId = b;
     }
 
+    Rect dup() {
+      return new Rect(rectId, rotated, baseRectId, sx, sy);
+    }
+
+    long sx, sy;
+
     this(int id, int r, int b, long x, long y) {
       rectId = id;
       rotated = r;
@@ -57,13 +64,6 @@ void problem() {
       sy = y;
     }
 
-    Rect dup() {
-      return new Rect(rectId, rotated, baseRectId, sx, sy);
-    }
-
-    long sx, sy;
-
-    int rotate() { return rotated = rotated ^ 1; }
     long w() { return WH[rectId][rotated]; }
     long h() { return WH[rectId][1 ^ rotated]; }
     long mins() { return WH[rectId].minElement; }
@@ -71,7 +71,9 @@ void problem() {
     long ex() { return sx + w(); }
     long ey() { return sy + h(); }
 
-    string asOutput() {
+    int rotate() { return rotated = rotated ^ 1; }
+
+    string toOutput() {
       return format("%s %s %s %s", rectId, rotated, "L", baseRectId);
     }
 
@@ -111,14 +113,6 @@ void problem() {
     }
   }
 
-  class State {
-    Rect[] rects;
-    long score;
-    
-    Rect[] bestRects;
-    long bestScore;
-  }
-
   Tuple!(long, int) bestRotate(Rect[] rects, long targetHeight) {
     long bestScore = long.max;
     int bestRotate;
@@ -126,17 +120,20 @@ void problem() {
     foreach(rn; 0..2 ^^ rects.length.to!int) {
       long score, height;
       auto rnt = rn;
+      long w;
       foreach(i, ref rect; rects) {
         auto r = rnt % 2;
         rnt /= 2;
 
         rect.rotated = r;
         height += rect.h();
+        w = max(w, rect.w());
         if (i > 0) {
           score += abs(rects[i - 1].w() - rect.w());
         }
       }
       score += abs(height - targetHeight) * 16;
+      if (rects[$ - 1].rectId == N - 1) score -= w * 0;
       if (bestScore.chmin(score)) bestRotate = rn;
     }
 
@@ -150,174 +147,87 @@ void problem() {
     return tuple(bestScore, bestRotate);
   }
 
-  long[] measureInteractive(Rect[] rects) {
-    rects.length.writeln;
-    foreach(r; rects) writeln(r.asOutput());
-    stdout.flush();
-    return scan!long(2);
-  }
-
-  long[] measure(Rect[] rects) {
-    long w, h;
-    foreach(i, r; rects) {
-      r.recalc(rects[0..i]);
-      w = max(w, r.ex);
-      h = max(h, r.ey);
-    }
-    return [w, h];
-  }
-
-  Rect[] allRects;
-  Tuple!(long[], Rect[]) testPack(long threshold, bool dryrun = false) {
+  long[] testPack(long threshold) {
     int pre = -1;
 
-    Rect[] queue;
+    N.writeln;
+    Rect[] queue, preQueue;
     long width;
     
     void resetColumn() {
       // queue.array.each!deb;
-      // foreach(qr; queue) writeln(qr.asOutput());
+      foreach(qr; queue) writeln(qr.toOutput());
       width = 0;
-      allRects ~= queue;
-      queue.length = 0;
+      swap(queue, preQueue);
+      queue = [];
     }
 
-    // int currentRectIndex;
-    // while(currentRectIndex < N) {
-    //   int nextMin, nextMax; {
-    //     long mih, mah;
-    //     foreach(wh; WH[currentRectIndex..$]) {
-    //       if (mih < threshold) {
-    //         mih += wh.maxElement;
-    //         nextMin++;
-    //       }
-    //       if (mah < threshold) {
-    //         mah += wh.minElement;
-    //         nextMax++;
-    //       }
-    //     }
-    //   }
-
-    //   Rect[] rects;
-    //   long bestScore = long.max;
-    //   int bestRotated;
-    //   // [nextMin + currentRectIndex, nextMax + currentRectIndex].deb;
-
-    //   foreach(nexts; max(0, nextMin - 1)..min(N - currentRectIndex + 1, nextMax + 1)) {
-    //     Rect[] testRects;
-    //     foreach(i, wh; WH[currentRectIndex..currentRectIndex + nexts].enumerate(0)) {
-    //       testRects ~= new Rect(i + currentRectIndex, 0, testRects.empty ? -1 : testRects.back.rectId);
-    //     }
-
-    //     auto tested = bestRotate(testRects, threshold);
-    //     if (bestScore.chmin(tested[0])) {
-    //       rects = testRects;
-    //       bestRotated = tested[1];
-    //     }
-    //   }
-    //   queue = rects;
-    //   // rects[0].overlaped(rects[1], 1, 79170).deb;
-    //   currentRectIndex += queue.length.to!int;
-    //   resetColumn();
-    // }
-    
-    foreach(i, whb; WH.enumerate(0)) {
-      auto rect = new Rect(i, 0, queue.empty ? -1 : queue.back.rectId);
-      width = max(width, rect.mins());
-      queue ~= rect;
-
-      long height;
-      foreach(ref qr; queue) {
-        if (abs(qr.w - width) > abs(qr.h - width)) qr.rotate();
-        height += qr.h;
+    int currentRectIndex;
+    while(currentRectIndex < N) {
+      int nextMin, nextMax; {
+        long mih, mah;
+        foreach(wh; WH[currentRectIndex..$]) {
+          if (mih < threshold) {
+            mih += wh.maxElement;
+            nextMin++;
+          }
+          if (mah < threshold) {
+            mah += wh.minElement;
+            nextMax++;
+          }
+        }
       }
 
-      if (height >= threshold) {
-        resetColumn();
+      Rect[] rects;
+      long bestScore = long.max;
+      int bestRotated;
+      [nextMin + currentRectIndex, nextMax + currentRectIndex].deb;
+
+      foreach(nexts; max(0, nextMin)..min(N - currentRectIndex + 1, nextMax + 1)) {
+        Rect[] testRects;
+        foreach(i, wh; WH[currentRectIndex..currentRectIndex + nexts].enumerate(0)) {
+          testRects ~= new Rect(i + currentRectIndex, 0, testRects.empty ? -1 : testRects.back.rectId);
+        }
+
+        auto tested = bestRotate(testRects, threshold);
+        if (bestScore.chmin(tested[0])) {
+          rects = testRects;
+          bestRotated = tested[1];
+        }
       }
+      queue = rects;
+      currentRectIndex += queue.length.to!int;
+      resetColumn();
+      stdout.flush();
     }
-    resetColumn();
-    auto m = measure(allRects);
-    return tuple(dryrun ? m : measureInteractive(allRects), allRects.dup);
+
+    return scan!long(2);
   }
 
-  const stepSize = baseHeight / 4000;
-
+  long threshold = WH.map!"a.maxElement".sum;
   long bestThreshold;
   long bestScore = long.max;
+  int tried;
 
-  enum STEPS = 1000;
-  foreach(step; -STEPS..STEPS + 1) {
-    long th = baseHeight + step*stepSize;
-    auto tested = testPack(th, true);
-    allRects.length = 0;
-    if (bestScore.chmin(tested[0].sum)) {
-      bestThreshold = th;
+  long stepSize = baseHeight * 100 / T / 200;
+  while(tried < T) {
+    if (elapsed(2800)) {
+      tried++;
+      writeln(0);
+      continue;
+    }
+    foreach(d; [-1, 1]) {
+      if (tried >= T - 1) break;
+
+      long th = baseHeight + d*stepSize*(tried/2);
+      long[] wh = testPack(th);
+      if (bestScore.chmin(wh.sum)) {
+        bestThreshold = th;
+      }
+      tried++;
+      if (tried == 1) break;
     }
   }
-
-  auto best = testPack(bestThreshold);
-  T--;
-
-  Rect[] curRects = best[1].map!"a.dup".array;
-  Rect[] snapshot = best[1].map!"a.dup".array;
-  long snapScore = best[0].sum;
-  int badCount;
-  int count;
-  while(!elapsed(2500)) {
-    count++;
-    auto target = uniform(0, N, RND);
-    curRects[target].rotate();
-
-    long w, h;
-    foreach(i; 0..N) {
-      curRects[i].recalc(curRects[0..i]);
-      w = max(w, curRects[i].ex);
-      h = max(h, curRects[i].ey);
-    }
-    if (w + h < snapScore) {
-      snapshot = curRects.map!"a.dup".array;
-      snapScore = w + h;
-      badCount = 0;
-      snapScore.deb;
-    } else if (w + h > snapScore * 1.2 || badCount >= 5) {
-      curRects = snapshot.map!"a.dup".array;
-      badCount = 0;
-    } else {
-      badCount++;
-    }
-  }
-
-  deb("burned: ", count);
-
-  // [bestScore, bestThreshold].deb;
-  // int tried; while(tried < T) {
-  //   if (elapsed(2500)) break;
-
-  //   foreach(d; [-1, 1]) {
-  //     long th = baseHeight + d*stepSize*(tried/2);
-  //     long[] wh = testPack(th);
-  //     allRects.length = 0;
-  //     if (bestScore.chmin(wh.sum)) {
-  //       bestThreshold = th;
-  //     }
-
-  //     tried++;
-  //     if (tried >= T) break;
-  //   }
-  // }
-
-  foreach(_; 0..T) {
-    writeln(snapshot.length);
-    foreach(r; snapshot) writeln(r.asOutput());
-    stdout.flush();
-    scan!long(2);
-  }
-
-  // foreach(_; 0..100000) foreach(i, rect; allRects) {
-  //   rect.recalc(allRects[0..i]);
-  //   // rect.asDimension.deb;
-  // }``
 }
 
 // ----------------------------------------------
