6d416e2ac70319fd41ea896850725b6ad0912ec5
allegrogiken <vvani06+dev@gmail.com>
2024-12-22 18:19:27 +0900

    ABC385
    - solved: ABCDE

---
+++ home/atcoder/1221_abc385/d.d
@@ -0,0 +1,173 @@
+void main() { runSolver(); }
+
+struct Move {
+  string d;
+  long c;
+}
+
+void problem() {
+  auto N = scan!int;
+  auto M = scan!int;
+  auto X = scan!long;
+  auto Y = scan!long;
+  auto XY = scan!long(N * 2).chunks(2).array;
+  auto DC = M.iota.map!(_ => Move(scan, scan!long)).array;
+
+  auto solve() {
+    RedBlackTree!long[long] cPerR, rPerC;
+    foreach(xy; XY) {
+      auto x = xy[0];
+      auto y = xy[1];
+      cPerR.require(y, new long[](0).redBlackTree);
+      cPerR[y].insert(x);
+      rPerC.require(x, new long[](0).redBlackTree);
+      rPerC[x].insert(y);
+    }
+
+    long ans;
+    foreach(move; DC) {
+      // move.deb;
+      if (move.d == "U" || move.d == "D") {
+        auto moved = Y;
+        if (move.d == "U") moved += move.c; else moved -= move.c;
+
+        if (X in rPerC) {
+          auto low = min(moved, Y);
+          auto high = max(moved, Y);
+
+          // [X, low, high].deb;
+          // rPerC[X].dup.deb;
+          long[] rem;
+          foreach(t; rPerC[X].upperBound(low - 1)) {
+            if (t > high) break;
+
+            // [X, t].deb;
+            rem ~= t;
+            cPerR[t].removeKey(X);
+            ans++;
+          }
+          rPerC[X].removeKey(rem);
+          // rPerC[X].dup.deb;
+        }
+
+        Y = moved;
+      } else {
+        auto moved = X;
+        if (move.d == "R") moved += move.c; else moved -= move.c;
+
+        if (Y in cPerR) {
+          auto low = min(moved, X);
+          auto high = max(moved, X);
+
+          // [Y, low, high].deb;
+          // cPerR[Y].dup.deb;
+          long[] rem;
+          foreach(t; cPerR[Y].upperBound(low - 1)) {
+            if (t > high) break;
+
+            // [t, Y].deb;
+            rem ~= t;
+            rPerC[t].removeKey(Y);
+            ans++;
+          }
+          cPerR[Y].removeKey(rem);
+          // cPerR[Y].dup.deb;
+        }
+
+        X = moved;
+      }
+    }
+
+    return [X, Y, ans];
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
+  }
+  return ret;
+}
+void deb(T ...)(T t){ debug t.writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+struct Eratosthenes {
+  bool[] isPrime;
+  int[] rawPrimes;
+  int[] spf;
+
+  this(int lim) {
+    isPrime = new bool[](lim + 1);
+    isPrime[2..$] = true;
+    spf = new int[](lim + 1);
+    spf[] = int.max;
+    spf[0..2] = 1;
+
+    foreach (i; 2..lim+1) {
+      if (isPrime[i]) {
+        spf[i] = i;
+
+        auto x = i*2;
+        while (x <= lim) {
+          isPrime[x] = false;
+          spf[x].chmin(i);
+          x += i;
+        }
+      }
+    }
+
+    foreach(p; 2..lim + 1) {
+      if (isPrime[p]) rawPrimes ~= p;
+    }
+  }
+
+  auto primes() { return rawPrimes.assumeSorted; }
+
+  int[int] factorize(int x) {
+    int[int] ret;
+    while(x > 1) {
+      ret[spf[x]]++;
+      x /= spf[x];
+    }
+    return ret;
+  }
+}
