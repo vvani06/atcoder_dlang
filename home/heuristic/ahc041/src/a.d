31841240724b59d12be2b362b84c18423d901a26
allegrogiken <vvani06+dev@gmail.com>
2025-02-02 18:56:26 +0900

    AHC041 ritsujirohさんの貪欲+ランダム実装

--- home/heuristic/ahc041/src/a.d
+++ home/heuristic/ahc041/src/a.d
@@ -17,110 +17,97 @@ void problem() {
   int[] A = scan!int(N);
   int[][] E = scan!int(2 * M).chunks(2).array;
   int[][] XY = scan!int(2 * N).chunks(2).array;
+  int[] idRandom = N.iota.array;
   
   struct Node {
-    int node, value;
+    int id, value, depth, from = -1;
+
+    inout int opCmp(inout Node other) {
+      return cmp(
+        [-value, idRandom[id]],
+        [-other.value, idRandom[other.id]],
+      );
+    }
   }
 
-  auto nodes = N.iota.map!(i => Node(i, A[i])).array.sort!"a.value > b.value";
-  int[][] graph = new int[][](N, 0);
-  foreach(e; E) {
-    graph[e[0]] ~= e[1];
-    graph[e[1]] ~= e[0];
+  struct Sim {
+    int score;
+    int[] ans;
   }
 
-  Tuple!(int, int[]) solve(int tried) {
-    if (tried == 0) {
-      foreach(ref g; graph) g.sort!((a, b) => A[a] < A[b]);
-    } else {
-      foreach(ref g; graph) g.randomShuffle(RND);
+  Sim simulate() {
+    Node[] nodes = N.iota.map!(i => Node(i, A[i])).array;
+    int[][] graph = new int[][](N, 0);
+    foreach(e; E) {
+      graph[e[0]] ~= e[1];
+      graph[e[1]] ~= e[0];
     }
 
-    bool[] used = new bool[](N);
     int[] roots = (-1).repeat(N).array;
-    int[] rootIds;
+    bool[] used = new bool[](N);
+    auto candidates = nodes.redBlackTree;
+    int score;
 
-    alias Item = Tuple!(int, "node", int, "depth");
-    auto heap = new Item[](0).heapify!"a.depth < b.depth";
+    foreach(baseDepth; 0..H + 1) {
+      int[] visits = new int[](N);
+      int[][] visitsTo = new int[][](N, 0);
 
-    auto availables = N.iota.map!(i => graph[i].redBlackTree).array;
+      foreach(node; candidates) {
+        bool[] visited = used.dup;
+        visited[node.id] = true;
+        for(auto queue = DList!Node(Node(node.id, 0, baseDepth)); !queue.empty;) {
+          auto cur = queue.front();
+          queue.removeFront();
+          visits[cur.id]++;
+          visitsTo[node.id] ~= cur.id;
+          if (cur.depth == H) continue;
 
-    foreach(leaf; nodes) {
-      if (used[leaf.node]) continue;
+          foreach(next; graph[cur.id]) {
+            if (visited[next]) continue;
 
-      used[leaf.node] = true;
-      int[] route;
-
-      int dfs(int cur, int pre, int depth) {
-        if (depth > H) return depth;
-
-        foreach(next; graph[cur]) {
-          if (next == pre || used[next]) continue;
-
-          roots[cur] = next;
-          used[next] = true;
-          route ~= next;
-          return dfs(next, cur, depth + 1);
-        }
-        return depth;
-      }
-
-      int maxDepth = dfs(leaf.node, leaf.node, 1);
-      foreach(depth, node; route.retro.enumerate(1)) heap.insert(Item(node, depth));
-
-      while(!heap.empty) {
-        Item item = heap.front;
-        heap.removeFront;
-
-        auto branch = item.node;
-        foreach(next; availables[branch].array) {
-          availables[branch].removeKey(next);
-          if (used[next]) continue;
-
-          roots[next] = branch;
-          used[next] = true;
-          if (item.depth < H) heap.insert(Item(next, item.depth + 1));
-          if (!availables[branch].empty) {
-            heap.insert(item);
-            break;
+            visited[next] = true;
+            queue.insertBack(Node(next, 0, cur.depth + 1));
           }
         }
       }
 
-      rootIds ~= route.empty ? leaf.node : route[$ - 1];
-    }
-
-    int[][] trees = new int[][](N, 0);
-    foreach(cur, root; roots.enumerate(0)) {
-      if (root != -1) trees[root] ~= cur;
-    }
-
-    int heightSum;
-    int score = 1;
-    foreach(root; rootIds) {
-      void countHeights(int cur, int depth) {
-        heightSum += depth;
-        score += depth * A[cur];
-        foreach(next; trees[cur]) countHeights(next, depth + 1);
+      int[] newRoots;
+      foreach(node; candidates) {
+        if (visitsTo[node.id].any!(n => visits[n] <= 1)) {
+          used[node.id] = true;
+          roots[node.id] = node.from;
+          newRoots ~= node.id;
+          score += node.value * (baseDepth + 1);
+        } else {
+          foreach(t; visitsTo[node.id]) visits[t]--;
+        }
       }
-      countHeights(root, 1);
+
+      candidates.clear();
+      foreach(nr; newRoots) {
+        foreach(next; graph[nr]) {
+          if (used[next]) continue;
+
+          nodes[next].from = nr;
+          candidates.insert(nodes[next]);
+        }
+      }
     }
-    (heightSum.to!real / N).deb;
-    score.deb;
-    // writefln("%(%s %)", roots);
-
-    return tuple(score, roots);
+    writefln("%(%s %)", roots);
+    return Sim(score, roots);
   }
 
-  int[] ans;
-  int bestScore;
-  int tried;
-  while(!elapsed(1800)) {
-    auto ret = solve(tried++);
-    if (bestScore.chmax(ret[0])) ans = ret[1];
-    break;
+  auto sim = simulate();
+  while(!elapsed(1850)) {
+    idRandom.randomShuffle(RND);
+
+    auto more = simulate();
+    if (sim.score < more.score) {
+      sim = more;
+    }
   }
-  writefln("%(%s %)", ans);
+
+  writefln("%(%s %)", sim.ans);
 }
 
 // ----------------------------------------------
@@ -154,51 +141,3 @@ void runSolver() {
 enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
-
-struct UnionFind {
-  int[] roots;
-  int[] sizes;
-  long[] weights;
- 
-  this(int size) {
-    roots = size.iota.array;
-    sizes = 1.repeat(size).array;
-    weights = 0L.repeat(size).array;
-  }
- 
-  int root(int x) {
-    if (roots[x] == x) return x;
-
-    const root = root(roots[x]);
-    weights[x] += weights[roots[x]];
-    return roots[x] = root;
-  }
-
-  int size(int x) {
-    return sizes[root(x)];
-  }
- 
-  bool unite(int x, int y, long w = 0) {
-    int rootX = root(x);
-    int rootY = root(y);
-    if (rootX == rootY) return weights[x] - weights[y] == w;
- 
-    if (sizes[rootX] < sizes[rootY]) {
-      swap(x, y);
-      swap(rootX, rootY);
-      w *= -1;
-    }
-
-    sizes[rootX] += sizes[rootY];
-    weights[rootY] = weights[x] - weights[y] - w;
-    roots[rootY] = rootX;
-    return true;
-  }
- 
-  bool same(int x, int y, int w = 0) {
-    int rootX = root(x);
-    int rootY = root(y);
- 
-    return rootX == rootY && weights[rootX] - weights[rootY] == w;
-  }
-}
