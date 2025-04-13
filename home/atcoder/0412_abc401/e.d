83f9ac6d57ba8ebd187e9380c205a4831ea0a04a
allegrogiken <vvani06+dev@gmail.com>
2025-04-13 20:42:57 +0900

    ABC401
    - solved: ABCDE
    - WA: Dx2

---
+++ home/atcoder/0412_abc401/e.d
@@ -0,0 +1,302 @@
+void main() { runSolver(); }
+
+void problem() {
+  auto N = scan!int;
+  auto M = scan!int;
+  auto E = scan!int(2 * M).chunks(2);
+
+  auto solve() {
+    alias Edge = Tuple!(int, "from", int, "to");
+    auto edges = M.iota.map!(i => Edge(E[i][0] - 1, E[i][1] - 1)).array;
+
+    auto uf = UnionFind(N);
+    foreach(e; edges) {
+      uf.unite(e.from, e.to);
+    }
+
+    Edge[][] edgesPerMaxNode = new Edge[][](N + 1, 0);
+    Edge[][] edgesPerMinNode = new Edge[][](N + 1, 0);
+    foreach(e; edges) {
+      edgesPerMaxNode[max(e.from, e.to)] ~= e;
+      edgesPerMinNode[min(e.from, e.to)] ~= e;
+    }
+
+    struct Nodes {
+      bool[int] nodes;
+
+      Nodes merge(Nodes other) {
+        foreach(i; other.nodes.keys) nodes[i] = true;
+        return Nodes(nodes);
+      }
+    }
+
+    bool[int] waitedNodes;
+    auto uf2 = UnionFindWith!Nodes(N);
+    foreach(i; 0..N) uf2.setExtra(i, Nodes([i: true]));
+
+    auto segtree = SegTree!("a & b", int)(new int[](N + 1), 1);
+    segtree.update(0, 1);
+
+    foreach(i; 0..N) {
+      foreach(e; edgesPerMaxNode[i]) {
+        auto pf = uf2.same(0, e.from);
+        auto pt = uf2.same(0, e.to);
+        auto fe = uf2.extra(e.from);
+        auto te = uf2.extra(e.to);
+
+        uf2.unite(e.from, e.to);
+        if (!pf && uf2.same(0, e.from)) {
+          foreach(x; fe.nodes.keys) segtree.update(x, true);
+        }
+        if (!pt && uf2.same(0, e.to)) {
+          foreach(x; te.nodes.keys) segtree.update(x, true);
+        }
+      }
+      foreach(e; edgesPerMinNode[i]) {
+        waitedNodes[e.from] = true;
+        waitedNodes[e.to] = true;
+      }
+
+      // (i + 1).iota.map!(x => uf2.same(0, x)).deb;
+      // uf2.extra(0).deb;
+      writeln(segtree.sum(0, i + 1) == 1 ? max(0, waitedNodes.length.to!int - i - 1) : -1);
+    }
+  }
+
+  outputForAtCoder(&solve);
+}
+
+// ----------------------------------------------
+
+import std;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+T[] compress(T)(T[] arr, T origin = T.init) { T[T] indecies; arr.dup.sort.uniq.enumerate(origin).each!((i, t) => indecies[t] = i); return arr.map!(t => indecies[t]).array; }
+long[] divisors(long n) { long[] ret; for (long i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+struct ModInt(uint MD) if (MD < int.max) {ulong v;this(string v) {this(v.to!long);}this(int v) {this(long(v));}this(long v) {this.v = (v%MD+MD)%MD;}void opAssign(long t) {v = (t%MD+MD)%MD;}static auto normS(ulong x) {return (x<MD)?x:x-MD;}static auto make(ulong x) {ModInt m; m.v = x; return m;}auto opBinary(string op:"+")(ModInt r) const {return make(normS(v+r.v));}auto opBinary(string op:"-")(ModInt r) const {return make(normS(v+MD-r.v));}auto opBinary(string op:"*")(ModInt r) const {return make((ulong(v)*r.v%MD).to!ulong);}auto opBinary(string op:"^^", T)(T r) const {long x=v;long y=1;while(r){if(r%2==1)y=(y*x)%MD;x=x^^2%MD;r/=2;} return make(y);}auto opBinary(string op:"/")(ModInt r) const {return this*memoize!inv(r);}static ModInt inv(ModInt x) {return x^^(MD-2);}string toString() const {return v.to!string;}auto opOpAssign(string op)(ModInt r) {return mixin ("this=this"~op~"r");}}
+alias MInt1 = ModInt!(10^^9 + 7);
+alias MInt9 = ModInt!(998_244_353);
+string asAnswer(T ...)(T t) {
+  string ret;
+  foreach(i, a; t) {
+    if (i > 0) ret ~= "\n";
+    alias A = typeof(a);
+    static if (isIterable!A && !is(A == string)) {
+      string[] rets;
+      foreach(b; a) rets ~= asAnswer(b);
+      static if (isInputRange!A) ret ~= rets.joiner(" ").to!string; else ret ~= rets.joiner("\n").to!string; 
+    } else {
+      static if (is(A == float) || is(A == double) || is(A == real)) ret ~= "%.16f".format(a);
+      else static if (is(A == bool)) ret ~= YESNO[a]; else ret ~= "%s".format(a);
+    }
+  }
+  return ret;
+}
+void deb(T ...)(T t){ debug t.writeln; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else asAnswer(fn()).writeln;
+}
+void runSolver() {
+  static import std.datetime.stopwatch;
+  enum BORDER = "==================================";
+  debug { BORDER.writeln; while(!stdin.eof) { "<<< Process time: %s >>>".writefln(std.datetime.stopwatch.benchmark!problem(1)); BORDER.writeln; } }
+  else problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
+
+struct UnionFindWith(T = UnionFindExtra) {
+  int[] roots;
+  int[] sizes;
+  long[] weights;
+  T[] extras;
+ 
+  this(int size) {
+    roots = size.iota.array;
+    sizes = 1.repeat(size).array;
+    weights = 0L.repeat(size).array;
+    extras = new T[](size);
+  }
+ 
+  this(int size, T[] ex) {
+    roots = size.iota.array;
+    sizes = 1.repeat(size).array;
+    weights = 0L.repeat(size).array;
+    extras = ex.dup;
+  }
+ 
+  int root(int x) {
+    if (roots[x] == x) return x;
+
+    const root = root(roots[x]);
+    weights[x] += weights[roots[x]];
+    return roots[x] = root;
+  }
+
+  int size(int x) {
+    return sizes[root(x)];
+  }
+
+  T extra(int x) {
+    return extras[root(x)];
+  }
+
+  T setExtra(int x, T t) {
+    return extras[root(x)] = t;
+  }
+ 
+  bool unite(int x, int y, long w = 0) {
+    int rootX = root(x);
+    int rootY = root(y);
+    if (rootX == rootY) return weights[x] - weights[y] == w;
+ 
+    if (sizes[rootX] < sizes[rootY]) {
+      swap(x, y);
+      swap(rootX, rootY);
+      w *= -1;
+    }
+
+    sizes[rootX] += sizes[rootY];
+    weights[rootY] = weights[x] - weights[y] - w;
+    extras[rootX] = extras[rootX].merge(extras[rootY]);
+    roots[rootY] = rootX;
+    return true;
+  }
+ 
+  bool same(int x, int y, int w = 0) {
+    int rootX = root(x);
+    int rootY = root(y);
+ 
+    return rootX == rootY && weights[rootX] - weights[rootY] == w;
+  }
+
+  auto dup() {
+    auto dupe = UnionFindWith!T(roots.length.to!int);
+    dupe.roots = roots.dup;
+    dupe.sizes = sizes.dup;
+    dupe.weights = weights.dup;
+    dupe.extras = extras.dup;
+    return dupe;
+  }
+}
+
+struct UnionFindExtra { UnionFindExtra merge(UnionFindExtra other) { return UnionFindExtra(); } }
+alias UnionFind = UnionFindWith!UnionFindExtra;
+
+struct SegTree(alias pred = "a + b", T = long) {
+  alias predFun = binaryFun!pred;
+  int size;
+  T[] data;
+  T monoid;
+ 
+  this(T[] src, T monoid = T.init) {
+    this.monoid = monoid;
+
+    for(int i = 2; i < 2L^^32; i *= 2) {
+      if (src.length <= i) {
+        size = i;
+        break;
+      }
+    }
+    
+    data = new T[](size * 2);
+    foreach(i, s; src) data[i + size] = s;
+    foreach_reverse(b; 1..size) {
+      data[b] = predFun(data[b * 2], data[b * 2 + 1]);
+    }
+  }
+ 
+  void update(int index, T value) {
+    int i = index + size;
+    data[i] = value;
+    while(i > 0) {
+      i /= 2;
+      data[i] = predFun(data[i * 2], data[i * 2 + 1]);
+    }
+  }
+
+  void add(int index, T value) {
+    update(index, get(index) + value);
+  }
+ 
+  T get(int index) {
+    return data[index + size];
+  }
+ 
+  T sum(int a, int b, int k = 1, int l = 0, int r = -1) {
+    if (r < 0) r = size;
+    
+    if (r <= a || b <= l) return monoid;
+    if (a <= l && r <= b) return data[k];
+ 
+    T leftValue = sum(a, b, 2*k, l, (l + r) / 2);
+    T rightValue = sum(a, b, 2*k + 1, (l + r) / 2, r);
+    return predFun(leftValue, rightValue);
+  }
+
+  T[] array() {
+    return size.iota.map!(i => get(i)).array;
+  }
+
+  int lowerBound(T border) {
+    return binarySearch((int t) => sum(0, t) < border, 0, size + 1);
+  }
+
+  int upperBound(T border) {
+    return binarySearch((int t) => sum(t, size) < border, size, -1);
+  }
+
+  private K binarySearch(K)(bool delegate(K) cond, K l, K r) { return binarySearch((K k) => k, cond, l, r); }
+  private T binarySearch(T, K)(K delegate(T) fn, bool delegate(K) cond, T l, T r) {
+    auto ok = l;
+    auto ng = r;
+    const T TWO = 2;
+  
+    bool again() {
+      static if (is(T == float) || is(T == double) || is(T == real)) {
+        return !ng.approxEqual(ok, 1e-08, 1e-08);
+      } else {
+        return abs(ng - ok) > 1;
+      }
+    }
+  
+    while(again()) {
+      const half = (ng + ok) / TWO;
+      const halfValue = fn(half);
+  
+      if (cond(halfValue)) {
+        ok = half;
+      } else {
+        ng = half;
+      }
+    }
+  
+    return ok;
+  }
+}
+
+long countInvertions(T)(T[] arr) {
+  auto segtree = SegTree!("a + b", long)(new long[](arr.length));
+  long ret;
+  long pre = -1;
+  int[] adds;
+  foreach(a; arr.enumerate(0).array.sort!"a[1] > b[1]") {
+    auto i = a[0];
+    auto n = a[1];
+    if (pre != n) {
+      foreach(ai; adds) segtree.update(ai, segtree.get(ai) + 1);   
+      adds.length = 0;
+    }
+    adds ~= i;
+    pre = n;
+    ret += segtree.sum(0, i);
+  }
+  return ret;
+}
