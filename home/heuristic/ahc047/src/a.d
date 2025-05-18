79359cf127f8ad1faf67215ca2011762bd3fdd7d
allegrogiken <vvani06+dev@gmail.com>
2025-05-18 20:50:25 +0900

    AHC047 3,592,224
    - トップ2件だけのことを考えたらなぜかうまくいった

--- home/heuristic/ahc047/src/a.d
+++ home/heuristic/ahc047/src/a.d
@@ -16,43 +16,42 @@ void problem() {
   
   enum ELM = "abcdef";
   auto P_MAX = SP.map!"a[1]".maxElement;
-  auto P_RATIO = P_MAX / 10;
+  auto P_RATIO = P_MAX / 5;
 
-  int[6][6] matrix;
-  foreach(sp; SP) {
-    char pre = sp[0][0];
-    foreach(next; sp[0][1..$]) {
-      matrix[pre - 'a'][next - 'a'] += max(1, sp[1] / P_RATIO) ^^ 4;
-      pre = next;
+  foreach(offset; 0..2) {
+    int[6][6] matrix;
+    int[] start = new int[](6);
+    foreach(sp; SP.sort!"a[1] > b[1]"[offset..offset + 1]) {
+      char pre = sp[0][0];
+      start[pre - 'a'] += max(1, sp[1] / P_RATIO) ^^ 4;
+      foreach(i; 0..6) matrix[i][pre - 'a'] += max(1, sp[1] / P_RATIO) ^^ 4;
+      foreach(next; sp[0][1..$]) {
+        matrix[pre - 'a'][next - 'a'] += max(1, sp[1] / P_RATIO) ^^ 5;
+        pre = next;
+      }
     }
-  }
-  foreach(m; matrix) m.deb;
+    // foreach(m; matrix) m.deb;
 
-  int[][] candidates = new int[][](6, 0);
-  foreach(i; 0..6) {
-    foreach(j; 0..6) {
-      candidates[i] ~= j.repeat(matrix[i][j]).array;
-    }
-  }
-
-  foreach(i; 0..6) {
-    auto s = [ELM[i % $]].to!string;
-    auto arr = 0.repeat(6).array ~ 1.repeat(6).array;
-    foreach(_; arr.sum..100) {
-      arr[0 + candidates[i].choice(RND)]++;
+    int[][] candidates = new int[][](6, 0);
+    foreach(i; 0..6) {
+      foreach(j; 0..6) {
+        candidates[i] ~= j.repeat(matrix[i][j]).array;
+      }
     }
 
-    writefln("%s %(%d %)", s, arr);
-  }
-
-  foreach(i; 0..6) {
-    auto s = [ELM[i % $]].to!string;
-    auto arr = 1.repeat(6).array ~ 0.repeat(6).array;
-    foreach(_; arr.sum..100) {
-      arr[6 + candidates[i].choice(RND)]++;
+    auto maxi = start.maxIndex;
+    string[] ans;
+    foreach(i; 0..6) {
+      auto s = [ELM[i % $]].to!string;
+      auto arr = 1.repeat(M).array;
+      foreach(_; arr.sum..100) {
+        arr[offset * 6 + candidates[i].choice(RND)]++;
+      }
+      arr.swapAt(offset * 6, offset * 6 + maxi);
+      ans ~= format("%s %(%d %)", s, arr);
     }
-
-    writefln("%s %(%d %)", s, arr);
+    ans.swapAt(0, maxi);
+    foreach(s; ans) writeln(s);
   }
 }
 
