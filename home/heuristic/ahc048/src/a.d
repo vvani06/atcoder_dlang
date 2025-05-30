7260f8999b6066d750b59398f764f262b865ef17
allegrogiken <vvani06+dev@gmail.com>
2025-05-30 22:02:39 +0900

    AHC048
    - 準備と簡単な実装をしてAC

---
+++ home/heuristic/ahc048/src/a.d
@@ -0,0 +1,73 @@
+void main() { runSolver(); }
+
+// ---------------------------------------------
+
+void problem() {
+  auto StartTime = MonoTime.currTime();
+  bool elapsed(int ms) { return (ms <= (MonoTime.currTime() - StartTime).total!"msecs"); }
+  auto seed = 983_741_243;
+  auto RND = Xorshift(seed);
+  enum long INF = long.max / 3;
+
+  int N = scan!int;
+  int K = scan!int;
+  int H = scan!int;
+  int T = scan!int;
+  int D = scan!int;
+  real[][] OWN = scan!real(3 * K).chunks(3).array;
+  real[][] TARGET = scan!real(3 * H).chunks(3).array;
+
+  foreach(_; 0..N) {
+    writefln("%(%s %)", 1.repeat(N - 1));
+  }
+  foreach(_; 1..N) {
+    writefln("%(%s %)", 1.repeat(N));
+  }
+
+  foreach(target; TARGET) {
+    real bestScore = int.max;
+    int bestOwn;
+    foreach(i, own; OWN.enumerate(0)) {
+      auto score = 3.iota.map!(i => pow(target[i] - own[i], 2)).sum;
+      if (bestScore.chmin(score)) {
+        bestOwn = i;
+      }
+    }
+
+    writefln("1 %s %s %s", 0, 0, bestOwn);
+    writefln("2 %s %s", 0, 0);
+  }
+}
+
+// ----------------------------------------------
+
+import std;
+import core.memory : GC;
+string scan(){ static string[] ss; while(!ss.length) ss = readln.chomp.split; string res = ss[0]; ss.popFront; return res; }
+T scan(T)(){ return scan.to!T; }
+T[] scan(T)(long n){ return n.iota.map!(i => scan!T()).array; }
+void deb(T ...)(lazy T t){ debug { write("# "); writeln(t); }}
+void debf(T ...)(lazy T t){ debug { write("# "); writefln(t); }}
+// void deb(T ...)(T t){ debug {  }}
+T[] divisors(T)(T n) { T[] ret; for (T i = 1; i * i <= n; i++) { if (n % i == 0) { ret ~= i; if (i * i != n) ret ~= n / i; } } return ret.sort.array; }
+bool chmin(T)(ref T a, T b) { if (b < a) { a = b; return true; } else return false; }
+bool chmax(T)(ref T a, T b) { if (b > a) { a = b; return true; } else return false; }
+string charSort(alias S = "a < b")(string s) { return (cast(char[])((cast(byte[])s).sort!S.array)).to!string; }
+ulong comb(ulong a, ulong b) { if (b == 0) {return 1;}else{return comb(a - 1, b - 1) * a / b;}}
+string toAnswerString(R)(R r) { return r.map!"a.to!string".joiner(" ").array.to!string; }
+void outputForAtCoder(T)(T delegate() fn) {
+  static if (is(T == float) || is(T == double) || is(T == real)) "%.16f".writefln(fn());
+  else static if (is(T == void)) fn();
+  else static if (is(T == string)) fn().writeln;
+  else static if (isInputRange!T) {
+    static if (!is(string == ElementType!T) && isInputRange!(ElementType!T)) foreach(r; fn()) r.toAnswerString.writeln;
+    else foreach(r; fn()) r.writeln;
+  }
+  else fn().writeln;
+}
+void runSolver() {
+  problem();
+}
+enum YESNO = [true: "Yes", false: "No"];
+
+// -----------------------------------------------
