eb4132a6a6cf83680a0b2b71b0c2d33d7e7f0bba
allegrogiken <vvani06@gmail.com>
2025-07-06 19:19:20 +0900

    AHC050 準備と最も単純な提出

--- home/ahc_tools/provision.sh
+++ home/ahc_tools/provision.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 set -e
 
+apt install libfontconfig1-dev pkg-config fontconfig libfontconfig-dev
+
 cd `dirname $0`
 TARGET=$1
 
@@ -31,6 +33,7 @@ declare -A TOOLS_URLS=(
   ["AHC047"]="https://img.atcoder.jp/ahc047/cHmFekjC.zip"
   ["AHC048"]="https://img.atcoder.jp/ahc048/lI5DXOAV.zip"
   ["AHC049"]="https://img.atcoder.jp/ahc049/LDUZCjLO.zip"
+  ["AHC050"]="https://img.atcoder.jp/ahc050/k1BmZE1o.zip"
 )
 
 rm -rf tools tools.zip
