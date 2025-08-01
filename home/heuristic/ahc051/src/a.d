8d7670cbb7cdc7454826deb77e6d2c7ce0b67b2c
allegrogiken <vvani06+dev@gmail.com>
2025-08-02 02:01:34 +0900

    AHC051 雑すぎる乱択実装 (21st)
    - 高さ3までの完全2分木を作る
    - ほぼ全要素をランダムアサインで、奇跡的に交差しないものを採択

--- home/heuristic/ahc051/src/a.d
+++ home/heuristic/ahc051/src/a.d
@@ -14,6 +14,7 @@ void problem() {
   }
 
   bool intersect(ref Coord p1, ref Coord p2, ref Coord q1, ref Coord q2) {
+    if (p1 == q1 || p1 == q2 || p2 == q1 || p2 == q2) return false;
     if (max(p1.x, p2.x) < min(q1.x, q2.x) ||
         max(q1.x, q2.x) < min(p1.x, p2.x) ||
         max(p1.y, p2.y) < min(q1.y, q2.y) ||
@@ -42,9 +43,93 @@ void problem() {
   int[][] S = scan!int(2 * M).chunks(2).array;
   real[][] P = scan!real(K * N).chunks(N).array;
 
-  writefln("%(%s %)", N.iota);
-  writefln("%s", 0);
-  foreach(_; 0..M) writefln("%s", -1);
+  int[][] bestAns;
+  real bestAnsScore = 0;
+
+  LIMIT: foreach(graphHeight; 1..4) {
+    int[][] graph = new int[][](M + N, 0);
+    foreach(i; 0..2^^graphHeight - 1) graph[i + N] = [i*2 + 1 + N, i*2 + 2 + N];
+    foreach(i; 2^^(graphHeight - 1) - 1..2^^graphHeight - 1) graph[i + N] = graph[i + N].map!(a => a % N).array; 
+
+    real bestScore = 0;
+    real[] bestScores;
+    int[] bestAssign;
+    foreach(_; 0..50000) {
+      if (elapsed(1500)) break;
+      auto assign = (-1).repeat(N).array ~ M.iota.map!(_ => uniform(0, K, RND)).array;
+
+      real[] scores;
+      foreach(item; 0..N) {
+        real[] p = (0.0L).repeat(N + M).array;
+        p[N] = 1.0;
+
+        foreach(node, nexts, sorter; zip(iota(N + M), graph, assign)) {
+          if (nexts.empty) continue;
+
+          p[nexts[0]] += p[node] * P[sorter][item];
+          p[nexts[1]] += p[node] * (1.0 - P[sorter][item]);
+        }
+        scores ~= p[item];
+      }
+        
+      if (bestScore.chmax(scores.sum)) {
+        bestAssign = assign;
+        bestScores = scores;
+      }
+    }
+
+    bestScore.deb;
+    bestScores.deb;
+    bestAssign[N..$].deb;
+
+    Coord[] coords = D.map!(c => Coord(c[0], c[1])).array ~ S.map!(c => Coord(c[0], c[1])).array ~ Coord(0, 5000);
+    int[] nodeMap = iota(N + M).array;
+
+    bool isOk;
+    MAIN: foreach(_; 0..200000) {
+      if (elapsed(1900)) break LIMIT;
+      nodeMap[0..N].randomShuffle(RND);
+      nodeMap[N..$].randomShuffle(RND);
+
+      int[][] lines = [[N + M, nodeMap[N]]];
+      foreach(i; N..N + M) {
+        if (graph[i].empty) continue;
+
+        auto from = nodeMap[i];
+        auto to1 = nodeMap[graph[i][0]];
+        lines ~= [from, to1];
+        auto to2 = nodeMap[graph[i][1]];
+        lines ~= [from, to2];
+      }
+
+      // lines.deb;
+      foreach(i; 0..lines.length - 1) foreach(j; i + 1..lines.length) {
+        if (intersect(coords[lines[i][0]], coords[lines[i][1]], coords[lines[j][0]], coords[lines[j][1]])) continue MAIN;
+      }
+      
+      isOk = true;
+      break;
+    }
+
+    if (isOk && bestAnsScore.chmax(bestScore)) {
+      bestAns.length = 0;
+      bestAns ~= N.iota.map!(i => nodeMap.countUntil(i).to!int).array;
+      bestAns ~= [nodeMap[N]];
+
+      int[][] sorts = [-1].repeat(M).array;
+      foreach(i; N..N + M) {
+        if (graph[i].empty) continue;
+        
+        auto m = nodeMap[i];
+        sorts[m - N] = [bestAssign[i], nodeMap[graph[i][0]], nodeMap[graph[i][1]]];
+      }
+      bestAns ~= sorts;
+    }
+  }
+
+  foreach(s; bestAns) {
+    writefln("%(%s %)", s);
+  }
 }
 
 // ----------------------------------------------
@@ -80,3 +165,4 @@ void runSolver() {
 enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
+
