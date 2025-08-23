48f8cde9ceb9f3bd8132187089ae45df4bc026a3
allegrogiken <vvani06+dev@gmail.com>
2025-08-23 17:31:27 +0900

    AHC052 345,552(152)
    - 左上に寄せて↑→↓→.. に操作する感じ
    - 終わってマスのあまりがあれば逆順にやる
    - 捜査の向きは乱択で時間いっぱい回す

--- home/heuristic/ahc052/src/a.d
+++ home/heuristic/ahc052/src/a.d
@@ -16,9 +16,9 @@ void problem() {
   string[] V = scan!string(N);
   string[] H = scan!string(N - 1);
 
-  enum DIRS = "LURD";
-
-  alias Coord = Tuple!(int, "r", int, "c");
+  enum DIRS = "LURDS";
+  enum MOVES_R = [0, -1, 0, 1, 0];
+  enum MOVES_C = [-1, 0, 1, 0, 0];
 
   class Grid {
     bool[][] left;
@@ -29,7 +29,7 @@ void problem() {
     this(string[] v, string[] h) {
       left = new bool[][](N, N);
       right = new bool[][](N, N);
-      foreach(r; 0..N) foreach(c; 0..N - 2) {
+      foreach(r; 0..N) foreach(c; 0..N - 1) {
         if (v[r][c] == '0') {
           right[r][c] = true;
           left[r][c + 1] = true;
@@ -37,30 +37,190 @@ void problem() {
       }
       up = new bool[][](N, N);
       down = new bool[][](N, N);
-      foreach(r; 0..N - 2) foreach(c; 0..N) {
+      foreach(r; 0..N - 1) foreach(c; 0..N) {
         if (h[r][c] == '0') {
           down[r][c] = true;
           up[r + 1][c] = true;
         }
       }
+
+      left[14].deb;
     }
 
-    bool canWalk(Coord coord, int dir) {
-      if (dir == 0) return left[coord.r][coord.c];
-      if (dir == 1) return up[coord.r][coord.c];
-      if (dir == 2) return right[coord.r][coord.c];
-      return down[coord.r][coord.c];
+    bool canWalk(int r, int c, int dir) {
+      if (dir == 0) return left[r][c];
+      if (dir == 1) return up[r][c];
+      if (dir == 2) return right[r][c];
+      if (dir == 3) return down[r][c];
+      return false;
+    }
+  }
+
+  class Controller {
+    int[][] dirs;
+
+    this(string[] commands) {
+      dirs = new int[][](K, M);
+      foreach(m; 0..M) foreach(k; 0..K) {
+        dirs[k][m] = DIRS.countUntil(commands[m][k]).to!int;
+      }
+    }
+
+    int dir(int robot, int command) {
+      return dirs[command][robot];
+    }
+
+    void outputAsAns() {
+      foreach(d; dirs) {
+        writefln("%s", d.map!(x => DIRS[x..x + 1]).joiner(" "));
+      }
+    }
+  }
+
+  class Simulator {
+    Grid grid;
+    Controller controller;
+    int[] operations;
+
+    bool[][] filled;
+    int rest;
+    int[] r;
+    int[] c;
+    
+    int valuesless;
+    int[] vr;
+    int[] vc;
+
+    this(Grid grid, Controller contoller, int[][] RC) {
+      this.grid = grid;
+      this.controller = contoller;
+      r = RC.map!"a[0]".array;
+      c = RC.map!"a[1]".array;
+      filled = new bool[][](N, N);
+      foreach(rr, cc; zip(r, c)) {
+        filled[rr][cc] = true;
+      }
+      rest = N^^2 - M;
+    }
+
+    void add(int command) {
+      if (rest <= 0) return;
+
+      int moved;
+      int fill;
+      foreach(m; 0..M) {
+        auto dir = controller.dir(m, command);
+        if (grid.canWalk(r[m], c[m], dir)) {
+          moved++;
+          r[m] += MOVES_R[dir];
+          c[m] += MOVES_C[dir];
+          if (!filled[r[m]][c[m]]) {
+            filled[r[m]][c[m]] = true;
+            rest--;
+            fill++;
+          }
+        }
+      }
+
+      if (moved > 0) {
+        operations ~= command;
+
+        if (fill > 0) {
+          if (rest <= 50) {
+            valuesless = 0;
+            vr = r.dup;
+            vc = c.dup;
+          }
+        } else {
+          valuesless++;
+        }
+      }
+    }
+
+    void add(int[] commands) {
+      foreach(c; commands) add(c);
+    }
+
+    int score() {
+      if (rest > 0) {
+        return N ^^ 2 - rest;
+      }
+
+      return 3 * N^^2 - operations.length.to!int;
+    }
+
+    void cutEdge() {
+      if (valuesless == 0 || vr.empty) return;
+
+      r = vr.dup;
+      c = vc.dup;
+      operations = operations[0..$ - valuesless];
+      valuesless = 0;
+    }
+
+    void outputAsAns() {
+      controller.outputAsAns();
+      writefln("%(%s %)", operations[0..min($, 2*N^^2)]);
     }
   }
 
   Grid grid = new Grid(V, H);
+  Simulator bestSim;
+  int bestScore;
+  int tried;
+  while(!elapsed(1900)) {
+    auto commands = M.iota.map!(_ => "LURDLURDLURDLURD"[uniform(0, 4, RND)..$]).array;
+    Controller controller = new Controller(commands);
 
-  foreach(m; 0..M) {
-    writefln("%s", K.iota.map!(_ => "LLRRDDUUS".array.choice(RND).to!string).joiner(" "));
+    auto mode1 = uniform(0, 2, RND);
+    auto mode2 = uniform(0, 2, RND);
+
+    Simulator sim = new Simulator(grid, controller, RC);
+    if (tried % 2) {
+      if (mode2) {
+        sim.add(2.repeat(N).array);
+        sim.add(3.repeat(N).array);
+      } else {
+        sim.add(3.repeat(N).array);
+        sim.add(2.repeat(N).array);
+      }
+      foreach(c; 0..N) {
+        if (mode1 == 0) sim.add(2);
+        sim.add(3.repeat(N).array);
+        sim.add(2);
+        sim.add(1.repeat(N).array);
+        if (mode1 == 1) sim.add(2);
+      }
+    } else {
+      if (mode2) {
+        sim.add(0.repeat(N).array);
+        sim.add(1.repeat(N).array);
+      } else {
+        sim.add(1.repeat(N).array);
+        sim.add(0.repeat(N).array);
+      }
+      foreach(c; 0..N) {
+        if (mode1 == 0) sim.add(2);
+        sim.add(3.repeat(N).array);
+        sim.add(2);
+        sim.add(1.repeat(N).array);
+        if (mode1 == 1) sim.add(2);
+      }
+    }
+
+    if (sim.rest > 0) {
+      sim.cutEdge();
+      foreach(c; sim.operations.retro) {
+        sim.add(c + 2);
+      }
+    }
+
+    tried++;
+    if (bestScore.chmax(sim.score)) bestSim = sim;
   }
-  foreach(i; 0..2 * N^^2) {
-    writeln(K.iota.choice(RND));
-  }
+
+  bestSim.deb;
+  bestSim.outputAsAns();
 }
 
 // ----------------------------------------------
