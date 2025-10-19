164aa156defed16317c2fe9afebb765dcf9ee7fb
allegrogiken <vvani06+dev@gmail.com>
2025-10-19 20:08:41 +0900

    AHC055 score = 8018406
    強い辺を優先して MST を作り、その順で宝箱を殴ってく感じの実装

--- home/heuristic/ahc055/src/a.d
+++ home/heuristic/ahc055/src/a.d
@@ -14,11 +14,92 @@ void problem() {
   int[] C = scan!int(N);
   int[][] A = scan!int(N ^^ 2).chunks(N).array;
 
-  foreach(i; 0..N) {
-    foreach(_; 0..H[i]) {
-      writefln("%s %s", -1, i);
+  struct Edge {
+    int from, to, value;
+
+    inout int opCmp(inout Edge other) {
+      return cmp(
+        [value, from, to],
+        [other.value, other.from, other.to],
+      );
     }
   }
+
+  int[] ins = new int[](N);
+  Edge[][] graph = new Edge[][](N, 0); {
+    Edge[] edges;
+    foreach(u; 0..N) foreach(v; 0..N) {
+      if (u == v) continue;
+
+      edges ~= Edge(u, v, A[u][v]);
+    }
+
+    auto uf = UnionFind(N);
+    foreach(e; edges.sort!"a > b") {
+      if (uf.same(e.from, e.to)) continue;
+
+      uf.unite(e.from, e.to);
+      graph[e.from] ~= e;
+      ins[e.to]++;
+    }
+  }
+
+  auto targets = (N.iota.filter!(i => ins[i] == 0).array).redBlackTree;
+  auto weapons = new int[](0).redBlackTree;
+  bool[] opened = new bool[](N);
+
+  auto healthBoxes = H.dup;
+  auto healthWeapons = C.dup;
+  auto methods = new Edge[](0).heapify;
+  Edge[] outputs;
+
+  int tries;
+  while(opened.canFind(false)) {
+    Edge method = {
+      while(!methods.empty) {
+        auto cur = methods.front;
+        if (healthWeapons[cur.from] <= 0 || opened[cur.to]) {
+          methods.removeFront();
+          continue;
+        }
+
+        return cur;
+      }
+
+      int mini = int.max;
+      int target;
+      foreach(t; targets) {
+        if (mini.chmin(healthBoxes[t])) target = t;
+      }
+
+      return Edge(-1, target, 1);
+    }();
+
+    if (method.from >= 0) {
+      if (--healthWeapons[method.from]) {
+      }
+    }
+
+    auto obtained = method.to;
+    healthBoxes[obtained] -= method.value;
+    if (healthBoxes[obtained] <= 0) {
+      targets.removeKey(obtained);
+      opened[obtained] = true;
+      foreach(to; 0..N) {
+        if (!opened[to]) methods.insert(Edge(obtained, to, A[obtained][to]));
+      }
+
+      foreach(e; graph[obtained]) {
+        if (--ins[e.to] == 0) {
+          if (!opened[e.to]) targets.insert(e.to);
+        }
+      }
+    }
+
+    outputs ~= method;
+  }
+
+  foreach(m; outputs) writefln("%s %s", m.from, m.to);
 }
 
 // ----------------------------------------------
@@ -54,3 +135,81 @@ void runSolver() {
 enum YESNO = [true: "Yes", false: "No"];
 
 // -----------------------------------------------
+
+struct UnionFindWith(T = UnionFindExtra) {
+  int[] roots;
+  int[] sizes;
+  long[] weights;
+  T[] extras;
+ 
+  this(int size) {
+    roots = size.iota.array;
+    sizes = 1.repeat(size).array;
+    weights = 0L.repeat(size).array;
+    extras = new T[](size);
+  }
+ 
+  this(int size, T[] ex) {
+    roots = size.iota.array;
+    sizes = 1.repeat(size).array;
+    weights = 0L.repeat(size).array;
+    extras = ex.dup;
+  }
+ 
+  int root(int x) {
+    if (roots[x] == x) return x;
+
+    const root = root(roots[x]);
+    weights[x] += weights[roots[x]];
+    return roots[x] = root;
+  }
+
+  int size(int x) {
+    return sizes[root(x)];
+  }
+
+  T extra(int x) {
+    return extras[root(x)];
+  }
+
+  T setExtra(int x, T t) {
+    return extras[root(x)] = t;
+  }
+ 
+  bool unite(int x, int y, long w = 0) {
+    int rootX = root(x);
+    int rootY = root(y);
+    if (rootX == rootY) return weights[x] - weights[y] == w;
+ 
+    if (sizes[rootX] < sizes[rootY]) {
+      swap(x, y);
+      swap(rootX, rootY);
+      w *= -1;
+    }
+
+    sizes[rootX] += sizes[rootY];
+    weights[rootY] = weights[x] - weights[y] - w;
+    extras[rootX] = extras[rootX].merge(extras[rootY]);
+    roots[rootY] = rootX;
+    return true;
+  }
+ 
+  bool same(int x, int y, int w = 0) {
+    int rootX = root(x);
+    int rootY = root(y);
+ 
+    return rootX == rootY && weights[rootX] - weights[rootY] == w;
+  }
+
+  auto dup() {
+    auto dupe = UnionFindWith!T(roots.length.to!int);
+    dupe.roots = roots.dup;
+    dupe.sizes = sizes.dup;
+    dupe.weights = weights.dup;
+    dupe.extras = extras.dup;
+    return dupe;
+  }
+}
+
+struct UnionFindExtra { UnionFindExtra merge(UnionFindExtra other) { return UnionFindExtra(); } }
+alias UnionFind = UnionFindWith!UnionFindExtra;
